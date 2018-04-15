97a3a77055ba70b106a0950ae854339402384368
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-04-16 01:58:44 +0200

    leetcode: add solution to problem 797

---
+++ leetcode/797_all_paths_from_source_to_target.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def bfs(self, graph, start, goal):
+        queue = [(start, [start])]
+        while queue:
+            (vertex, path) = queue.pop(0)
+            for next in set(graph[vertex]) - set(path):
+                if next == goal:
+                    yield path + [next]
+                else:
+                    queue.append((next, path + [next]))
+
+    def allPathsSourceTarget(self, graph):
+        return list(self.bfs(graph, 0, len(graph) - 1))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert sorted([[0,1,3],[0,2,3]]) == sorted(solution.allPathsSourceTarget([[1,2], [3], [3], []]))
