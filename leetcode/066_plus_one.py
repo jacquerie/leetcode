c191869ee17a96124c14858004ebf08a2ed79536
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-06 00:07:48 +0200

    leetcode: add solution to problem 066

---
+++ leetcode/066_plus_one.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        number = int(''.join(str(digit) for digit in digits))
+        return [int(digit) for digit in str(number + 1)]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [1, 2, 4] == solution.plusOne([1, 2, 3])
+    assert [4, 3, 2, 2] == solution.plusOne([4, 3, 2, 1])
