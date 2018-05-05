59bae57de82a02a1f48d1853ce63ba865a9c5a25
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-05 19:51:39 +0200

    leetcode: add solution to problem 784

---
+++ leetcode/784_letter_case_permutation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from itertools import product
+
+
+class Solution(object):
+    def letterCasePermutation(self, S):
+        return list(set(''.join(s) for s in product(*zip(S.lower(), S.upper()))))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert sorted(['a1b2', 'a1B2', 'A1b2', 'A1B2']) == sorted(solution.letterCasePermutation('a1b2'))
+    assert sorted(['3z4', '3Z4']) == sorted(solution.letterCasePermutation('3z4'))
+    assert sorted(['12345']) == sorted(solution.letterCasePermutation('12345'))
