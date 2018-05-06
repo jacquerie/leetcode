95e0f8faa4e323abd14a51fed577e73eca879e60
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-06 22:19:15 +0200

    leetcode: add solution to problem 747

---
+++ leetcode/747_largest_number_at_least_twice_of_others.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def dominantIndex(self, nums):
+        max_num = max(nums)
+
+        for num in nums:
+            if 2 * num > max_num and num != max_num:
+                return -1
+
+        return nums.index(max_num)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.dominantIndex([3, 6, 1, 0])
+    assert -1 == solution.dominantIndex([1, 2, 3, 4])
