df2347ac32a1766e5062c21ed216170217de2013
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-06 23:11:28 +0200

    leetcode: add solution to problem 824

---
+++ leetcode/824_goat_latin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+VOWELS = 'AEIOUaeiou'
+
+
+class Solution(object):
+    def toGoatLatin(self, S):
+        result = []
+
+        for i, word in enumerate(S.split(), 1):
+            result.append(self._toGoatLatin(word) + ('a' * i))
+
+        return ' '.join(result)
+
+    def _toGoatLatin(self, word):
+        if word[0] in VOWELS:
+            return word + 'ma'
+        else:
+            return word[1:] + word[0] + 'ma'
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'Imaa peaksmaaa oatGmaaaa atinLmaaaaa' == solution.toGoatLatin('I speak Goat Latin')
