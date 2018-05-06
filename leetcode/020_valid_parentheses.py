f776d87fe5bacd124cf42b3c9bb3e72550cc3986
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-06 19:10:53 +0200

    leetcode: add solution to problem 020

---
+++ leetcode/020_valid_parentheses.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def isValid(self, s):
+        stack = deque()
+
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '[':
+                stack.append(']')
+            elif c == '{':
+                stack.append('}')
+            elif c == ')' or c == ']' or c == '}':
+                try:
+                    top = stack.pop()
+                    if top != c:
+                        return False
+                except IndexError:
+                    return False
+
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isValid('()')
+    assert solution.isValid('()[]{}')
+    assert not solution.isValid('(]')
+    assert not solution.isValid('([)]')
+    assert solution.isValid('{[]}')
+    assert not solution.isValid(']')
+    assert not solution.isValid('[')
