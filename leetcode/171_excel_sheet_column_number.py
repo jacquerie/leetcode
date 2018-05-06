cec7d672b7125b6d4773ace387cd3c27283500de
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-06 17:20:38 +0200

    leetcode: add solution to problem 171

---
+++ leetcode/171_excel_sheet_column_number.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import string
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        result = 0
+
+        for c in s:
+            result *= 26
+            result += string.ascii_uppercase.index(c) + 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.titleToNumber('A')
+    assert 28 == solution.titleToNumber('AB')
+    assert 701 == solution.titleToNumber('ZY')
