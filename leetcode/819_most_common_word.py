c7eec50d21ef779c174befbcdfa657dcaaa194f7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-08 22:18:55 +0200

    leetcode: add solution to problem 819

---
+++ leetcode/819_most_common_word.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import re
+from collections import Counter
+
+
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        paragraph = paragraph.lower()
+        paragraph = re.sub('[^a-z ]+', '', paragraph)
+
+        words = Counter(paragraph.split())
+        banned = set(banned)
+
+        for word, _ in words.most_common():
+            if word not in banned:
+                return word
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'ball' == solution.mostCommonWord('Bob hit a ball, the hit BALL flew far after it was hit.', ['hit'])
