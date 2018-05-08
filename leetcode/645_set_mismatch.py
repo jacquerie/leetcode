a8514ffaadb930db1362269fc5f6b9804c2aa309
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-08 12:55:36 +0200

    leetcode: add solution to problem 645

---
+++ leetcode/645_set_mismatch.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def findErrorNums(self, nums):
+        repeated = (Counter(nums) - Counter(range(1, len(nums) + 1))).keys()
+        missing = list(set(range(1, len(nums) + 1)) - set(nums))
+
+        return repeated + missing
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    print(solution.findErrorNums([1, 2, 2, 4]))
+    assert [2, 3] == solution.findErrorNums([1, 2, 2, 4])
