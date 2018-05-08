39de5842b0b002b94d5c1528ace1b049e4d22558
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-08 19:39:27 +0200

    leetcode: add solution to problem 013

---
+++ leetcode/013_roman_to_integer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+ROMAN_NUMERAL_MAP = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        result = 0
+
+        for i, c in enumerate(s):
+            if i > 0 and ROMAN_NUMERAL_MAP[c] > ROMAN_NUMERAL_MAP[s[i - 1]]:
+                result += ROMAN_NUMERAL_MAP[c] - 2 * ROMAN_NUMERAL_MAP[s[i - 1]]
+            else:
+                result += ROMAN_NUMERAL_MAP[c]
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.romanToInt('III')
+    assert 4 == solution.romanToInt('IV')
+    assert 9 == solution.romanToInt('IX')
+    assert 58 == solution.romanToInt('LVIII')
+    assert 1994 == solution.romanToInt('MCMXCIV')
