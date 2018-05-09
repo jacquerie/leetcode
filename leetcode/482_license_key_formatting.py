a57d0796f5673bb94481702fc86a769e1da99bb3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-09 21:23:18 +0200

    leetcode: add solution to problem 482

---
+++ leetcode/482_license_key_formatting.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def licenseKeyFormatting(self, S, K):
+        S = S.upper()
+        S = ''.join(S.split('-'))
+        S = S[::-1]
+
+        parts = [S[i:i + K] for i in range(0, len(S), K)]
+        return '-'.join(parts)[::-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert '5F3Z-2E9W' == solution.licenseKeyFormatting('5F3Z-2e-9-w', 4)
+    assert '2-5G-3J' == solution.licenseKeyFormatting('2-5g-3-J', 2)
