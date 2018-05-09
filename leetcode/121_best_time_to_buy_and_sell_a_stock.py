555481b3cb3e5b1334cf7c960be7c953452ae543
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-10 00:56:55 +0200

    leetcode: add solution to problem 121

---
+++ leetcode/121_best_time_to_buy_and_sell_a_stock.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        minimum_price = float('inf')
+        maximum_profit = 0
+
+        for price in prices:
+            if price < minimum_price:
+                minimum_price = price
+            if price - minimum_price > maximum_profit:
+                maximum_profit = price - minimum_price
+
+        return maximum_profit
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.maxProfit([7, 1, 5, 3, 6, 4])
+    assert 0 == solution.maxProfit([7, 6, 4, 3, 1])
