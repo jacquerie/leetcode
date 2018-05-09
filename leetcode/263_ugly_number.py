ddee9ed06f08d9bc2903e5ee3d316ee1c91e6e15
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-09 20:00:55 +0200

    leetcode: add solution to problem 263

---
+++ leetcode/263_ugly_number.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def isUgly(self, num):
+        if num == 0:
+            return False
+
+        for i in (2, 3, 5):
+            while num % i == 0:
+                num //= i
+
+        return num == 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isUgly(1)
+    assert solution.isUgly(6)
+    assert solution.isUgly(8)
+    assert not solution.isUgly(14)
+    assert not solution.isUgly(0)
