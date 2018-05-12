11044ea95c3e50047baceb1d2ae4559c6c20733f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-12 23:12:40 +0200

    leetcode: add solution to problem 575

---
+++ leetcode/575_distribute_candies.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def distributeCandies(self, candies):
+        return min(len(candies) / 2, len(set(candies)))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.distributeCandies([1, 1, 2, 2, 3, 3])
+    assert 2 == solution.distributeCandies([1, 1, 2, 3])
