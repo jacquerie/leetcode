ebca5fd2238f1441c395dc888ef6a19ce1720001
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-15 19:01:34 +0200

    leetcode: add solution to problem 009

---
+++ leetcode/009_palindrome_number.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        digits = []
+        while x:
+            digits.append(x % 10)
+            x //= 10
+
+        return digits == digits[::-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isPalindrome(121)
+    assert not solution.isPalindrome(-121)
+    assert not solution.isPalindrome(10)
