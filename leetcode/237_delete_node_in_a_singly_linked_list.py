711a9c7d3848096a4cee3c847921a2cb55515d63
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-17 23:21:12 +0200

    leetcode: add solution to problem 237

---
+++ leetcode/237_delete_node_in_a_singly_linked_list.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __eq__(self, other):
+        return self.val == other.val and self.next == other.next
+
+    def __str__(self):
+        return '<ListNode val=%d>' % self.val
+
+
+class Solution(object):
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    l0_0 = ListNode(1)
+    l0_1 = ListNode(2)
+    l0_2 = ListNode(3)
+    l0_3 = ListNode(4)
+    l0_0.next = l0_1
+    l0_1.next = l0_2
+    l0_2.next = l0_3
+
+    l1_0 = ListNode(1)
+    l1_1 = ListNode(2)
+    l1_2 = ListNode(4)
+    l1_0.next = l1_1
+    l1_1.next = l1_2
+
+    assert solution.deleteNode(l0_2) is None
+    assert l0_0 == l1_0
