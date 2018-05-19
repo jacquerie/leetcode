ae8155c6021dec9e84faf6ae5cf27d55517a6a32
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-19 20:54:45 +0200

    leetcode: add solution to problem 160

---
+++ leetcode/160_intersection_of_two_linked_lists.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __eq__(self, other):
+        return self.val == other.val and self.next == other.next
+
+    def __str__(self):
+        return '<ListNode val=%d>' % self.val
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        if headA is None or headB is None:
+            return None
+
+        lenA, lenB = 1, 1
+        currA, currB = headA, headB
+
+        while currA.next is not None:
+            currA = currA.next
+            lenA += 1
+
+        while currB.next is not None:
+            currB = currB.next
+            lenB += 1
+
+        currA, currB = headA, headB
+
+        if lenA >= lenB:
+            for _ in range(lenA - lenB):
+                currA = currA.next
+        else:
+            for _ in range(lenB - lenA):
+                currB = currB.next
+
+        while (currA.next is not None and currB.next is not None and currA != currB):
+            currA = currA.next
+            currB = currB.next
+
+        if currA == currB:
+            return currA
+        return None
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    c1 = ListNode('c1')
+    c2 = ListNode('c2')
+    c3 = ListNode('c3')
+    c1.next = c2
+    c2.next = c3
+
+    a1 = ListNode('a1')
+    a2 = ListNode('a2')
+    a1.next = a2
+    a2.next = c1
+
+    b1 = ListNode('b1')
+    b2 = ListNode('b2')
+    b3 = ListNode('b3')
+    b1.next = b2
+    b2.next = b3
+    b3.next = c1
+
+    assert c1 == solution.getIntersectionNode(a1, b1)
