d4b3f84c9ed1887edddcf186116813d002d8937f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-19 14:55:22 +0200

    leetcode: add solution to problem 739

---
+++ leetcode/739_daily_temperatures.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        result = [0] * len(temperatures)
+
+        stack = deque()
+        for i, temperature in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temperature:
+                j = stack.pop()
+                result[j] = i - j
+            stack.append(i)
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [1, 1, 4, 2, 1, 1, 0, 0] == solution.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
