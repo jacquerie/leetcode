a4c9f0cc9de40d6af311af766f5efe1221112195
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-19 16:18:42 +0200

    leetcode: add c++ solution to problem 392

---
+++ leetcode/392_is_subsequence.cpp
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence (string s, string t) {
+        if (s.empty()) {
+            return true;
+        }
+
+        int i = 0, l = s.length();
+        for (auto c: t) {
+            if (c == s[i]) {
+                i++;
+                if (i == l) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(solution.isSubsequence("abc", "ahbgdc"));
+    assert(not solution.isSubsequence("axc", "ahbgdc"));
+    assert(solution.isSubsequence("", "ahbgdc"));
+    assert(solution.isSubsequence("a", "ab"));
+}
