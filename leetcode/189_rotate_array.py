fe87188e590d0018bf2557ab8ab23167693cfcf4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-20 02:16:56 +0200

    leetcode: add solution to problem 189

---
+++ leetcode/189_rotate_array.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    nums = [1, 2, 3, 4, 5, 6, 7]
+
+    assert solution.rotate(nums, 3) is None
+    assert [5, 6, 7, 1, 2, 3, 4] == nums
