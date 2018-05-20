56338da318af97269ab6a2407a4ebae10f77b6d1
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-20 19:02:43 +0200

    leetcode: add solution to problem 443

---
+++ leetcode/443_string_compression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def compress(self, chars):
+        result = []
+        for char, group in itertools.groupby(chars):
+            result.append(char)
+            length = len(list(group))
+            if length > 1:
+                result.append(str(length))
+
+        chars[:] = list(''.join(result))
+        return len(chars)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    chars = ['a', 'a', 'b', 'b', 'c', 'c', 'c']
+
+    assert 6 == solution.compress(chars)
+    assert ['a', '2', 'b', '2', 'c', '3'] == chars
+
+    chars = ['a']
+
+    assert 1 == solution.compress(chars)
+    assert ['a'] == chars
+
+    chars = ['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']
+
+    assert 4 == solution.compress(chars)
+    assert ['a', 'b', '1', '2'] == chars
