099364016863c15efaae730b0475cdcb9cab5e32
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-22 01:44:10 +0200

    leetcode: add c++ solution to problem 001

---
+++ leetcode/001_two_sum.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum (vector<int>& nums, int target) {
+        map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = seen.find(target - nums[i]);
+            if (it != seen.end()) {
+                return {it->second, i};
+            }
+
+            seen[nums[i]] = i;
+        }
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    vector<int> nums = {2, 7, 11, 15};
+
+    vector<int> expected = {0, 1};
+    vector<int> result = solution.twoSum(nums, 9);
+
+    assert(expected == result);
+}
