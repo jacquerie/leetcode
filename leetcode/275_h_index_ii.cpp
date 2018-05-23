e9c8bb2ee8fd490a5937249fc337f2c4a24dc86b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-24 01:26:10 +0200

    leetcode: add c++ solution to problem 275

---
+++ leetcode/275_h_index_ii.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int hIndex (vector<int>& citations) {
+        int n = citations.size();
+        int mid, first = 0, last = n - 1;
+
+        while (first <= last) {
+            mid = (first + last) / 2;
+            if (citations[mid] >= n - mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+
+        return n - first;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    vector<int> citations = {0, 1, 3, 5, 6};
+
+    assert(3 == solution.hIndex(citations));
+}
