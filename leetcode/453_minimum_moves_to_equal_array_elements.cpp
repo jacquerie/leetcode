80711a1e86cd404808cb4ab50dd0d93bbebc010a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-24 20:29:35 +0200

    leetcode: add c++ solution to problem 453

---
+++ leetcode/453_minimum_moves_to_equal_array_elements.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minMoves (vector<int>& nums) {
+        int count = 0, min = INT_MAX, sum = 0;
+
+        for (auto num: nums) {
+            if (num < min) {
+                min = num;
+            }
+
+            count++;
+            sum = sum + num;
+        }
+
+        return sum - count * min;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    vector<int> nums = {1, 2, 3};
+
+    assert(3 == solution.minMoves(nums));
+}
