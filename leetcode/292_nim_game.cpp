690286baded68d6de94c9cbc82ea43429a05c69b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-24 20:50:25 +0200

    leetcode: add c++ solution to problem 292

---
+++ leetcode/292_nim_game.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim (int n) {
+        return n % 4 != 0;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(!solution.canWin(4));
+}
