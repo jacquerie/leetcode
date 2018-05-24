d8a3bdafdcd4b09d7e23b2fd3d04f43ab334175f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-25 01:10:47 +0200

    leetcode: add c++ solution to problem 319

---
+++ leetcode/319_bulb_switcher.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int bulbSwitch (int n) {
+        return sqrt(n);
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(1 == solution.bulbSwitch(3));
+}
