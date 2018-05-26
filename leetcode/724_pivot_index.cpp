47b36b8959885b08e7d336b13518f513ed32b7be
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-26 19:04:48 +0200

    leetcode: add c++ solution to problem 724

---
+++ leetcode/724_pivot_index.cpp
@@ -0,0 +1,33 @@
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex (vector<int>& nums) {
+        int totalSum = 0;
+        for (auto num: nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    vector<int> nums = {1, 7, 3, 6, 5, 6};
+
+    assert(3 == solution.pivotIndex(nums));
+}
