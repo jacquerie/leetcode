fce12a6a1ee5c65a3075b680878d58c07cf4e396
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-26 21:08:56 +0200

    leetcode: add c++ solution to problem 371

---
+++ leetcode/371_sum_of_two_integers.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    int getSum (int a, int b) {
+        while (b) {
+            int carry = a & b;
+            a ^= b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(3 == solution.getSum(1, 2));
+}
