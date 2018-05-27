4046b8e1426f36c133d4225b2b4b0fb6b2406ecd
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-27 17:16:01 +0200

    leetcode: add c++ solution to problem 007

---
+++ leetcode/007_reverse_integer.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    int reverse (int x) {
+        int previous, result = 0;
+
+        while (x) {
+            previous = result;
+
+            result *= 10;
+            result += x % 10;
+            if (result / 10 != previous) {
+                return 0;
+            }
+
+            x /= 10;
+        }
+
+        return result;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(321 == solution.reverse(123));
+    assert(-321 == solution.reverse(-123));
+    assert(21 == solution.reverse(120));
+}
