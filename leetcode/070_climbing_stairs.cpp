94c767fbcc7e52c625bf6ecb82832dacff0601a8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-28 20:48:10 +0200

    leetcode: add c++ solution to problem 070

---
+++ leetcode/070_climbing_stairs.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+
+class Solution {
+public:
+    int climbStairs (int n) {
+        int previous = 1, current = 1;
+
+        for (int i = 1; i < n; i++) {
+            int tmp = current;
+            current += previous;
+            previous = tmp;
+        }
+
+        return current;
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    assert(1 == solution.climbStairs(1));
+    assert(2 == solution.climbStairs(2));
+    assert(3 == solution.climbStairs(3));
+    assert(5 == solution.climbStairs(4));
+    assert(8 == solution.climbStairs(5));
+}
