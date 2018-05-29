78301dff44eaae9a2567cfb43f693c6f34086026
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-29 23:28:08 +0200

    leetcode: add solution to problem 048

---
+++ leetcode/048_rotate_image.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+
+    assert solution.rotate(matrix) is None
+    assert [
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3],
+    ] == matrix
