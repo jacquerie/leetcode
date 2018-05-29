1b16d6162e229ba86d8ea642848837a8e37c1453
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-29 20:49:52 +0200

    leetcode: add c++ solution to problem 841

---
+++ leetcode/841_keys_and_rooms.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canVisitAllRooms (vector<vector<int>>& rooms) {
+        unordered_set<int> visited = {0};
+
+        queue<int> queue;
+        queue.push(0);
+
+        while (!queue.empty()) {
+            auto i = queue.front();
+            queue.pop();
+
+            for (auto j: rooms[i]) {
+                if (visited.find(j) == visited.end()) {
+                    visited.insert(j);
+                    queue.push(j);
+                }
+            }
+        }
+
+        return visited.size() == rooms.size();
+    }
+};
+
+int main () {
+    auto solution = Solution();
+
+    vector<vector<int>> rooms = {{1}, {2}, {3}, {}};
+
+    assert(solution.canVisitAllRooms(rooms));
+}
