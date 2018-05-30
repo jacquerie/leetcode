5ba0676161ae8afdd8bd20ccb2c90b92f0dc5fe4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-30 21:20:36 +0200

    leetcode: add c++ solution to problem 075

---
+++ leetcode/075_sort_colors.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0, n = nums.size() - 1;
+
+        while (j <= n) {
+            if (nums[j] == 0) {
+                swap(nums[i++], nums[j++]);
+            } else if (nums[j] == 1) {
+                j++;
+            } else if (nums[j] == 2) {
+                swap(nums[j], nums[n--]);
+            }
+        }
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    vector<int> expected = {0, 0, 1, 1, 2, 2};
+    solution.sortColors(nums);
+    assert(expected == nums);
+}
