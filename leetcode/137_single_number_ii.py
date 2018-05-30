166ab98b1be3b38c6321732627b2250c12136ce7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-05-30 23:37:15 +0200

    leetcode: add solution to problem 137

---
+++ leetcode/137_single_number_ii.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def fromBase3(self, digits):
+        return int(''.join(str(digit) for digit in reversed(digits)), 3)
+
+    def sumBase3(self, digits):
+        return sum(digit for digit in digits if digit) % 3
+
+    def toBase3(self, num):
+        digits = []
+        while num:
+            digits.append(num % 3)
+            num //= 3
+        return digits
+
+    def singleNumber(self, nums):
+        min_num = min(nums)
+        nums = [num + abs(min_num) for num in nums]
+
+        nums_in_base3 = [self.toBase3(num) for num in nums]
+        sums_in_base3 = [self.sumBase3(el) for el in itertools.izip_longest(*nums_in_base3)]
+
+        return self.fromBase3(sums_in_base3) - abs(min_num)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.singleNumber([2, 2, 3, 2])
+    assert 99 == solution.singleNumber([0, 1, 0, 1, 0, 1, 99])
+    assert -4 == solution.singleNumber([-2, -2, 1, 1, -3, 1, -3, -3, -4, -2])
