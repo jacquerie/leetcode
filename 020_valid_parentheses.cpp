11294cf692f9b090683ac680f319b71013298ecf
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-01 00:11:33 +0200

    leetcode: add c++ solution to problem 020

---
+++ 020_valid_parentheses.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+ public:
+    int isValid(string s) {
+        stack<char> stack;
+
+        for (auto c : s) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                char top = stack.top();
+                stack.pop();
+
+                if (top != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.size() == 0;
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    assert(solution.isValid("()"));
+    assert(solution.isValid("()[]{}"));
+    assert(!solution.isValid("(]"));
+    assert(!solution.isValid("([)]"));
+    assert(solution.isValid("{[]}"));
+    assert(!solution.isValid("]"));
+    assert(!solution.isValid("["));
+}
