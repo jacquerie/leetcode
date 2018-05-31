0756dcbd724d077276a5078943bb50905567db64
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-01 00:55:40 +0200

    leetcode: add solution to problem 290

---
+++ leetcode/290_word_pattern.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        words = str.split()
+        if len(pattern) != len(words):
+            return False
+
+        seen_chars, seen_words = {}, {}
+        for char, word in zip(pattern, words):
+            if char in seen_chars and seen_chars[char] != word:
+                return False
+            elif word in seen_words and seen_words[word] != char:
+                return False
+            seen_chars[char] = word
+            seen_words[word] = char
+
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.wordPattern('abba', 'dog cat cat dog')
+    assert not solution.wordPattern('abba', 'dog cat cat fish')
+    assert not solution.wordPattern('aaaa', 'dog cat cat dog')
+    assert not solution.wordPattern('abba', 'dog dog dog dog')
+    assert not solution.wordPattern('abba', 'dog cat cat')
+    assert not solution.wordPattern('abb', 'dog cat cat dog')
+    assert not solution.wordPattern('', 'beef')
