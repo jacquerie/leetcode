a9bbbea515133613b9a2cb6f6f793e6385515624
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-02 21:08:57 +0200

    leetcode: add solution to problem 049

---
+++ leetcode/049_group_anagrams.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = defaultdict(list)
+
+        for str in strs:
+            anagrams[''.join(sorted(str))].append(str)
+
+        return anagrams.values()
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
+
+    expected = [
+        ['eat', 'tea', 'ate'],
+        ['tan', 'nat'],
+        ['bat'],
+    ]
+    result = solution.groupAnagrams(strs)
+
+    assert sorted(expected) == sorted(result)
