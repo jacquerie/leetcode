8201ff73b377d327c78bbe815fddb2bced1cdbb1
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-03 00:18:34 +0200

    leetcode: add solution to problem 381

---
+++ leetcode/381_insert_delete_getrandom_o1_duplicates_allowed.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import random
+from collections import defaultdict
+
+
+class RandomizedCollection(object):
+    def __init__(self):
+        self.indices = defaultdict(list)
+        self.elements = []
+        self.length = 0
+
+    def insert(self, val):
+        result = True
+        if val in self.indices:
+            result = False
+        self.elements.append(val)
+        self.indices[val].append(self.length)
+        self.length += 1
+        return result
+
+    def remove(self, val):
+        result = False
+        if val in self.indices:
+            result = True
+            index = self.indices[val].pop()
+            if not self.indices[val]:
+                del self.indices[val]
+            self.elements[index] = None
+        return result
+
+    def getRandom(self):
+        result = random.choice(self.elements)
+        while result is None:
+            result = random.choice(self.elements)
+        return result
+
+
+if __name__ == '__main__':
+    obj = RandomizedCollection()
+
+    assert obj.insert(1)
+    assert not obj.insert(1)
+    assert obj.insert(2)
+    assert obj.getRandom() in [1, 2]
+    assert obj.remove(1)
+    assert obj.getRandom() in [1, 2]
