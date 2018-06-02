bbf0bacdf32ddd2304d2cac02d5c75075dd92f9b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-02 22:19:41 +0200

    leetcode: add c++ solution to problem 141

---
+++ leetcode/141_linked_list_cycle.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <cstddef>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    explicit ListNode(int x): val(x), next(NULL) {}
+};
+
+class Solution {
+ public:
+    bool hasCycle(ListNode* head) {
+        auto slow = head, fast = head;
+
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    auto l0_0 = ListNode(1);
+    auto l0_1 = ListNode(2);
+    auto l0_2 = ListNode(3);
+    auto l0_3 = ListNode(4);
+    auto l0_4 = ListNode(5);
+    l0_4.next = &l0_3;
+    l0_3.next = &l0_4;
+    l0_2.next = &l0_3;
+    l0_1.next = &l0_2;
+    l0_0.next = &l0_1;
+
+    assert(solution.hasCycle(&l0_0));
+}
