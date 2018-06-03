bda3abf63bf7a68a8560ba37abe9682fdd83c955
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-03 14:30:03 +0200

    leetcode: add c++ solution to problem 844

---
+++ leetcode/844_backspace_string_compare.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+ public:
+    bool backspaceCompare(string S, string T) {
+        return stringToStack(S) == stringToStack(T);
+    }
+
+ private:
+     stack<char> stringToStack(string S) {
+         stack<char> stack;
+
+         for (auto c : S) {
+             if (c == '#') {
+                 if (!stack.empty()) {
+                     stack.pop();
+                 }
+             } else {
+                 stack.push(c);
+             }
+         }
+
+         return stack;
+     }
+};
+
+int main() {
+    auto solution = Solution();
+
+    assert(solution.backspaceCompare("ab#c", "ad#c"));
+    assert(solution.backspaceCompare("ab##", "c#d#"));
+    assert(solution.backspaceCompare("a##c", "#a#c"));
+    assert(!solution.backspaceCompare("a#c", "b"));
+}
