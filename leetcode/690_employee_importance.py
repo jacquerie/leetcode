76d6efa75ef3d04cfdb30d14b4a9941008b57511
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-03 02:21:37 +0200

    leetcode: add solution to problem 690

---
+++ leetcode/690_employee_importance.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        employees_map = {}
+        for employee in employees:
+            employees_map[employee.id] = employee
+
+        result = 0
+
+        queue = deque([id])
+        while queue:
+            employee = employees_map[queue.pop()]
+            result += employee.importance
+            for subordinate in employee.subordinates:
+                queue.append(subordinate)
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    employees = [
+        Employee(1, 5, [2, 3]),
+        Employee(2, 3, []),
+        Employee(3, 3, []),
+    ]
+
+    assert 11 == solution.getImportance(employees, 1)
