a3740b6c63915c67788068f8ad57960dea0c8187
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-03 11:41:00 +0200

    leetcode: add c++ solution to problem 498

---
+++ leetcode/498_diagonal_traverse.cpp
@@ -0,0 +1,49 @@
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty() || matrix[0].empty()) {
+            return result;
+        }
+
+        int m = matrix.size(), n = matrix[0].size();
+
+        for (int k = 0; k < m + n - 1; k++) {
+            if (k % 2 == 0) {
+                for (int i = k; i >= 0; i--) {
+                    if (i < m && k - i < n) {
+                        result.push_back(matrix[i][k - i]);
+                    }
+                }
+            } else {
+                for (int i = 0; i <= k; i++) {
+                    if (i < m && k - i < n) {
+                        result.push_back(matrix[i][k - i]);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    vector<int> expected = {1, 2, 4, 7, 5, 3, 6, 8, 9};
+    vector<int> result = solution.findDiagonalOrder(matrix);
+
+    assert(expected == result);
+}
