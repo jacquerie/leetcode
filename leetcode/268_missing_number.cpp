8e12cd940a0651defd82120c230c1a32fd2860c3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-20 17:21:50 +0200

    leetcode: add c++ solution to problem 268

---
+++ leetcode/268_missing_number.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Jacopo Notarstefano
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+    int missingNumber(vector<int>& nums) {
+        int expected_sum = (nums.size() * (nums.size() + 1)) / 2;
+        int actual_sum = accumulate(nums.begin(), nums.end(), 0);
+
+        return expected_sum - actual_sum;
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    vector<int> nums = {3, 0, 1};
+
+    assert(2 == solution.missingNumber(nums));
+}
