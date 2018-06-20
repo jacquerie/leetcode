06c8c3ba3db2df863d2897ab3be9caed424a9bfa
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-20 18:17:41 +0200

    leetcode: add solution to problem 017

---
+++ leetcode/017_letters_combinations_of_a_phone_number.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from itertools import product
+
+DIGIT_LETTERS_MAP = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+}
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+
+        return [''.join(el) for el in product(*(DIGIT_LETTERS_MAP[digit] for digit in digits))]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'] == solution.letterCombinations('23')
