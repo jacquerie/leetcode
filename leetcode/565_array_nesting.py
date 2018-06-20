a65e347ffed495a82e36ae9293ac5682cb8bf693
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-21 01:05:28 +0200

    leetcode: improve solution to problem 565

--- leetcode/565_array_nesting.py
+++ leetcode/565_array_nesting.py
@@ -3,37 +3,22 @@
 from __future__ import absolute_import, division, print_function
 
 
-class UnionFind(object):
-    def __init__(self, n):
-        self.ids = list(range(n))
-        self.lengths = [1] * n
-
-    def root(self, i):
-        j = i
-        while (j != self.ids[j]):
-            self.ids[j] = self.ids[self.ids[j]]
-            j = self.ids[j]
-        return j
-
-    def union(self, p, q):
-        i = self.root(p)
-        j = self.root(q)
-        if i == j:
-            return
-        if self.lengths[i] < self.lengths[j]:
-            self.ids[i] = j
-            self.lengths[j] += self.lengths[i]
-        else:
-            self.ids[j] = i
-            self.lengths[i] += self.lengths[j]
-
-
 class Solution(object):
     def arrayNesting(self, nums):
-        union_find = UnionFind(len(nums))
+        result = 0
+
         for num in nums:
-            union_find.union(num, nums[num])
-        return max(union_find.lengths)
+            if num is None:
+                continue
+            current, length = num, 0
+            while nums[current] is not None:
+                previous = current
+                current = nums[current]
+                nums[previous] = None
+                length += 1
+            result = max(result, length)
+
+        return result
 
 
 if __name__ == '__main__':
