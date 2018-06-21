ad68c9d5b35d373b06096f63e84a233bec8d16ae
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-22 00:57:11 +0200

    leetcode: add c++ solution to problem 034

---
+++ leetcode/034_search_for_a_range.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2018 Jacopo Notarstefano
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto first = lower_bound(nums.begin(), nums.end(), target);
+        auto last = upper_bound(nums.begin(), nums.end(), target);
+
+        if (first != nums.end() && *first == target) {
+            return {first - nums.begin(), last - nums.begin() - 1};
+        } else {
+            return {-1, -1};
+        }
+    }
+};
+
+int main() {
+    auto solution = Solution();
+
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+
+    vector<int> expected = {3, 4};
+    vector<int> result = solution.searchRange(nums, 8);
+
+    assert(expected == result);
+}
