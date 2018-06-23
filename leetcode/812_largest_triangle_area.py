d744977dce02332195dc389dfc9dd4e0a5494d75
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-06-23 14:39:40 +0200

    leetcode: improve solution to problem 812

--- leetcode/812_largest_triangle_area.py
+++ leetcode/812_largest_triangle_area.py
@@ -2,21 +2,17 @@
 
 from __future__ import absolute_import, division, print_function
 
+import itertools
+
 
 class Solution(object):
     def largestTriangleArea(self, points):
         result = 0
 
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                for k in range(j, len(points)):
-                    x_a, y_a = points[i]
-                    x_b, y_b = points[j]
-                    x_c, y_c = points[k]
-
-                    area = abs((x_a - x_c) * (y_b - y_a) - (x_a - x_b) * (y_c - y_a)) / 2
-                    if area > result:
-                        result = area
+        for (x_a, y_a), (x_b, y_b), (x_c, y_c) in itertools.combinations(points, 3):
+            area = abs((x_a - x_c) * (y_b - y_a) - (x_a - x_b) * (y_c - y_a)) / 2
+            if area > result:
+                result = area
 
         return result
 
