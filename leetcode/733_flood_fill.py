76715db650c4d21260627d055d3e281b1b920b36
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-07-24 16:31:24 +0200

    leetcode: add solution to problem 733

---
+++ leetcode/733_flood_fill.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        nr, nc = len(image), len(image[0])
+        result = [[image[i][j] for j in xrange(nc)] for i in xrange(nr)]
+
+        queue = deque([(sr, sc)])
+        while queue:
+            r, c = queue.pop()
+            result[r][c] = newColor
+            if 0 < r and image[r][c] == image[r - 1][c] and result[r - 1][c] != newColor:
+                queue.append((r - 1, c))
+            if r < nr - 1 and image[r][c] == image[r + 1][c] and result[r + 1][c] != newColor:
+                queue.append((r + 1, c))
+            if 0 < c and image[r][c] == image[r][c - 1] and result[r][c - 1] != newColor:
+                queue.append((r, c - 1))
+            if c < nc - 1 and image[r][c] == image[r][c + 1] and result[r][c + 1] != newColor:
+                queue.append((r, c + 1))
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [
+        [2, 2, 2],
+        [2, 2, 0],
+        [2, 0, 1],
+    ] == solution.floodFill([
+        [1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 1],
+    ], 1, 1, 2)
