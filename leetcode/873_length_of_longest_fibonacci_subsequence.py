bff33d5b5a2847f9e6934351083989c4d5987590
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-07-25 09:48:27 +0200

    leetcode: add solution to problem 873

---
+++ leetcode/873_length_of_longest_fibonacci_subsequence.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def lenLongestFibSubseq(self, A):
+        result = 2
+
+        A_set = set(A)
+        for x, y in itertools.combinations(A, 2):
+            partial = 2
+            while x + y in A_set:
+                x, y = y, x + y
+                partial += 1
+            result = max(result, partial)
+
+        return result if result > 2 else 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.lenLongestFibSubseq([1, 2, 3, 4, 5, 6, 7, 8])
+    assert 3 == solution.lenLongestFibSubseq([1, 3, 7, 11, 12, 14, 18])
