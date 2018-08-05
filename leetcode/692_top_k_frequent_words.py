cfe0b3140fd5529a82bac2e08a43d4801add1064
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-05 18:03:59 +0200

    leetcode: add solution to problem 692

---
+++ leetcode/692_top_k_frequent_words.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import heapq
+from collections import Counter
+
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        counter = Counter(words)
+
+        most_common = []
+        for el in counter.items():
+            heapq.heappush(most_common, (-el[1], el[0]))
+
+        return [el[1] for el in heapq.nsmallest(k, most_common)]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert ['i', 'love'] == solution.topKFrequent(['i', 'love', 'leetcode', 'i', 'love', 'coding'], 2)
+    assert ['the', 'is', 'sunny', 'day'] == solution.topKFrequent(['the', 'day', 'is', 'sunny', 'the', 'the', 'the', 'sunny', 'is', 'is'], 4)
+    assert ['a'] == solution.topKFrequent(['aaa', 'aa', 'a'], 1)
