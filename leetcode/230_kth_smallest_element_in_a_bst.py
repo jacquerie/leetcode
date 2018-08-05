2768558cc4e69ac2c95012cae94bccc623db0350
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-06 00:02:45 +0200

    leetcode: add solution to problem 230

---
+++ leetcode/230_kth_smallest_element_in_a_bst.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        def _inorderTraversal(root, result):
+            if root.left is not None:
+                _inorderTraversal(root.left, result)
+            result.append(root.val)
+            if root.right is not None:
+                _inorderTraversal(root.right, result)
+
+        result = []
+        _inorderTraversal(root, result)
+        return result[k - 1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(3)
+    t0_1 = TreeNode(1)
+    t0_2 = TreeNode(4)
+    t0_3 = TreeNode(2)
+    t0_1.right = t0_3
+    t0_0.left = t0_1
+    t0_0.right = t0_2
+
+    assert 1 == solution.kthSmallest(t0_0, 1)
