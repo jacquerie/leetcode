e6870bd6a128758bca008ba3c787939470e86b67
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-06 18:10:18 +0200

    leetcode: add solution to problem 139

---
+++ leetcode/139_word_break.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        if not s:
+            return True
+
+        wordSet = set(wordDict)
+        result = [False] * (len(s) + 1)
+
+        for i in xrange(len(s) + 1):
+            if not result[i] and s[:i + 1] in wordSet:
+                result[i] = True
+            if result[i]:
+                for j in xrange(i + 1, len(s) + 1):
+                    if not result[j] and s[i + 1:j + 1] in wordSet:
+                        result[j] = True
+
+        return result[-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.wordBreak('leetcode', ['leet', 'code'])
+    assert solution.wordBreak('applepenapple', ['apple', 'pen'])
+    assert not solution.wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat'])
+    assert not solution.wordBreak(150 * 'a' + 'b', ['a', 'aa'])
