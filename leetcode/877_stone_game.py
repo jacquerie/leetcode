d60cef1010ca2e1dc10c497d3d690be0117a4fad
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-08 00:10:17 +0200

    leetcode: add solution to problem 877

---
+++ leetcode/877_stone_game.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def stoneGame(self, piles):
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.stoneGame([5, 3, 4, 5])
