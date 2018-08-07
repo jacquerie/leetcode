9a50b47ba08616ab299946011f1d3ea809902ae0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-07 14:33:23 +0200

    leetcode: add solution to problem 885

---
+++ leetcode/885_boats_to_save_people.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+
+        result = 0
+
+        i, j = 0, len(people) - 1
+        while i <= j:
+            result += 1
+            if i == j:
+                break
+            elif people[i] + people[j] > limit:
+                j -= 1
+            else:
+                i += 1
+                j -= 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.numRescueBoats([1, 2], 3)
+    assert 3 == solution.numRescueBoats([3, 2, 2, 1], 3)
+    assert 4 == solution.numRescueBoats([3, 5, 3, 4], 5)
