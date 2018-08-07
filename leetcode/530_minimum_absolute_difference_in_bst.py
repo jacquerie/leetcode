4482d230bb272075ecbcc8099aa1ddb904f7c587
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-07 14:04:48 +0200

    leetcode: add solution to problem 530

---
+++ leetcode/530_minimum_absolute_difference_in_bst.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def getMinimumDifference(self, root):
+        def _inOrderTraversal(root, previous, result):
+            if root is None:
+                return previous, result
+
+            previous, result = _inOrderTraversal(root.left, previous, result)
+            if previous:
+                result = min(result, root.val - previous.val)
+            return _inOrderTraversal(root.right, root, result)
+
+        _, result = _inOrderTraversal(root, None, float('inf'))
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(1)
+    t0_1 = TreeNode(3)
+    t0_2 = TreeNode(2)
+    t0_1.left = t0_2
+    t0_0.right = t0_1
+
+    assert 1 == solution.getMinimumDifference(t0_0)
