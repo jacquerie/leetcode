009e2142022303dcd7b817f3a2a95010ef278a28
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-07 11:25:58 +0200

    leetcode: add solution to problem 712

---
+++ leetcode/712_minimum_ascii_delete_sum_for_two_strings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def minimumDeleteSum(self, s1, s2):
+        result = [[0 for j in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i == 0 and j == 0:
+                    result[i][j] = 0
+                elif i == 0:
+                    result[i][j] = sum(ord(c) for c in s2[:j])
+                elif j == 0:
+                    result[i][j] = sum(ord(c) for c in s1[:i])
+                else:
+                    if s1[i - 1] == s2[j - 1]:
+                        result[i][j] = result[i - 1][j - 1]
+                    else:
+                        result[i][j] = min(
+                            result[i - 1][j] + ord(s1[i - 1]),
+                            result[i][j - 1] + ord(s2[j - 1]),
+                        )
+
+        return result[len(s1)][len(s2)]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 231 == solution.minimumDeleteSum('sea', 'eat')
+    assert 403 == solution.minimumDeleteSum('delete', 'leet')
