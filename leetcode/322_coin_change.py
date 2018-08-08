663e64e5be86d7f746abd807b5becfadfcd592d1
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-08 20:00:34 +0200

    leetcode: add solution to problem 322

---
+++ leetcode/322_coin_change.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        if amount == 0:
+            return 0
+
+        result = [0] * (amount + 1)
+
+        for i in xrange(amount + 1):
+            partial = float('inf')
+            for coin in coins:
+                if i - coin == 0:
+                    partial = 1
+                elif i - coin > 0:
+                    partial = min(partial, 1 + result[i - coin])
+            result[i] = partial
+
+        return -1 if result[-1] == float('inf') else result[-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.coinChange([1, 2, 5], 11)
+    assert -1 == solution.coinChange([2], 3)
