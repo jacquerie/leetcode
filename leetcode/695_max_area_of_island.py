4f14689040712e27ff032d53239e6be7d4a9b4cb
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-09 14:32:37 +0200

    leetcode: add solution to problem 695

---
+++ leetcode/695_max_area_of_island.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        result = 0
+        if not grid:
+            return result
+
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                if grid[i][j]:
+                    result = max(result, self.measureAndDeleteIsland(grid, i, j))
+
+        return result
+
+    def measureAndDeleteIsland(self, grid, i, j):
+        result = set()
+
+        queue = deque([(i, j)])
+        while queue:
+            x, y = queue.pop()
+            grid[x][y] = 0
+            if 0 < x and grid[x - 1][y]:
+                queue.append((x - 1, y))
+            if 0 < y and grid[x][y - 1]:
+                queue.append((x, y - 1))
+            if x < len(grid) - 1 and grid[x + 1][y]:
+                queue.append((x + 1, y))
+            if y < len(grid[0]) - 1 and grid[x][y + 1]:
+                queue.append((x, y + 1))
+            result.add((x, y))
+
+        return len(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.maxAreaOfIsland([
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    ])
+    assert 0 == solution.maxAreaOfIsland([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ])
+    assert 4 == solution.maxAreaOfIsland([
+        [1, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 1, 1],
+    ])
