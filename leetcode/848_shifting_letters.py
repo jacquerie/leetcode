fa6bed60a5757ff7e8d02c32f7224fd4459f75f6
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-09 08:20:06 +0200

    leetcode: add solution to problem 848

---
+++ leetcode/848_shifting_letters.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def shiftingLetters(self, S, shifts):
+        result = []
+
+        current_shift = 0
+        for shift in shifts:
+            current_shift += shift
+            current_shift %= 26
+
+        for i, c in enumerate(S):
+            result.append(self.shiftLetter(c, current_shift))
+            current_shift -= shifts[i]
+            current_shift %= 26
+
+        return ''.join(result)
+
+    def shiftLetter(self, c, shift):
+        return chr(ord('a') + (ord(c) + shift - ord('a')) % 26)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'rpl' == solution.shiftingLetters('abc', [3, 5, 9])
