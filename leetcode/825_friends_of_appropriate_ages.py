6ef30b2b94fc4b0b1b6669bef9cf9f806d839fe7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-12 01:01:54 +0200

    leetcode: add solution to problem 825

---
+++ leetcode/825_friends_of_appropriate_ages.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def numFriendRequests(self, ages):
+        result = 0
+
+        counts = Counter(ages)
+        for age1, count1 in counts.iteritems():
+            for age2, count2 in counts.iteritems():
+                if age1 // 2 + 7 < age2 <= age1:
+                    result += count1 * (count2 - (1 if age1 == age2 else 0))
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.numFriendRequests([16, 16])
+    assert 2 == solution.numFriendRequests([16, 17, 18])
+    assert 3 == solution.numFriendRequests([20, 30, 100, 110, 120])
+    assert 3 == solution.numFriendRequests([108, 115, 5, 24, 82])
