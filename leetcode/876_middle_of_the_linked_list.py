d1f47bbd3896dcc42dd98e85230f92110b900a8b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-12 19:21:55 +0200

    leetcode: add solution to problem 876

---
+++ leetcode/876_middle_of_the_linked_list.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __eq__(self, other):
+        return self.val == other.val and self.next == other.next
+
+
+class Solution(object):
+    def middleNode(self, head):
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    l0_0 = ListNode(1)
+    l0_1 = ListNode(2)
+    l0_2 = ListNode(3)
+    l0_3 = ListNode(4)
+    l0_4 = ListNode(5)
+    l0_3.next = l0_4
+    l0_2.next = l0_3
+    l0_1.next = l0_2
+    l0_0.next = l0_1
+
+    l1_0 = ListNode(3)
+    l1_1 = ListNode(4)
+    l1_2 = ListNode(5)
+    l1_1.next = l1_2
+    l1_0.next = l1_1
+
+    assert l1_0 == solution.middleNode(l0_0)
