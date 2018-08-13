3894ccf801f1e90843fe1f693ca26448f5f95d92
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-13 17:07:19 +0200

    leetcode: add solution to problem 697

---
+++ leetcode/697_degree_of_an_array.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import namedtuple
+
+Occurrence = namedtuple('Occurrence', ['min', 'max', 'count'])
+
+
+class Solution(object):
+    def findShortestSubArray(self, nums):
+        occurrences = {}
+        for i, num in enumerate(nums):
+            if num in occurrences:
+                occurrence = occurrences[num]
+                occurrences[num] = Occurrence(
+                    min=occurrence.min, max=i, count=occurrence.count + 1)
+            else:
+                occurrences[num] = Occurrence(min=i, max=i, count=1)
+
+        result = float('inf')
+
+        max_count = max(occurrence.count for occurrence in occurrences.values())
+        for occurrence in occurrences.values():
+            if occurrence.count == max_count:
+                result = min(result, occurrence.max - occurrence.min + 1)
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.findShortestSubArray([1, 2, 2, 3, 1])
+    assert 6 == solution.findShortestSubArray([1, 2, 2, 3, 1, 4, 2])
