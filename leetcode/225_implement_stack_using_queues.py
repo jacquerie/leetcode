a43c80a31352d3048b0460da0859f8caba0fa18d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-13 14:11:47 +0200

    leetcode: add solution to problem 225

---
+++ leetcode/225_implement_stack_using_queues.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class MyStack(object):
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x):
+        self.queue.appendleft(x)
+        for _ in range(len(self.queue) - 1):
+            self.queue.appendleft(self.queue.pop())
+
+    def pop(self):
+        return self.queue.pop()
+
+    def top(self):
+        return self.queue[-1]
+
+    def empty(self):
+        return not self.queue
+
+
+if __name__ == '__main__':
+    obj = MyStack()
+
+    obj.push(1)
+    obj.push(2)
+    assert 2 == obj.top()
+    assert 2 == obj.pop()
+    assert not obj.empty()
