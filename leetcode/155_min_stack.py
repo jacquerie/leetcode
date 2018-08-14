a468f12d4bafab416311ddea2aa874123b3ce0fb
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-14 12:56:59 +0200

    leetcode: add solution to problem 155

---
+++ leetcode/155_min_stack.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class MinStack(object):
+    def __init__(self):
+        self.mins = deque()
+        self.stack = deque()
+
+    def push(self, x):
+        if not self.mins or x <= self.mins[-1]:
+            self.mins.append(x)
+        self.stack.append(x - self.mins[-1])
+
+    def pop(self):
+        if not self.stack[-1]:
+            self.mins.pop()
+        self.stack.pop()
+
+    def top(self):
+        return self.mins[-1] + self.stack[-1]
+
+    def getMin(self):
+        return self.mins[-1]
+
+
+if __name__ == '__main__':
+    obj = MinStack()
+
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+    assert -3 == obj.getMin()
+    obj.pop()
+    assert 0 == obj.top()
+    assert -2 == obj.getMin()
+
+    obj = MinStack()
+
+    obj.push(0)
+    obj.push(1)
+    obj.push(0)
+    assert 0 == obj.getMin()
+    obj.pop()
+    assert 0 == obj.getMin()
