50ab7c64cd073bcf0f6dece23afc658ba69a0f09
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-15 15:30:12 +0200

    leetcode: add solution to problem 150

---
+++ leetcode/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = deque()
+
+        for token in tokens:
+            if token == '+':
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(op1 + op2)
+            elif token == '-':
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(op1 - op2)
+            elif token == '*':
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(op1 * op2)
+            elif token == '/':
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(int(op1 / float(op2)))
+            else:
+                stack.append(int(token))
+
+        return stack[-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 9 == solution.evalRPN(['2', '1', '+', '3', '*'])
+    assert 6 == solution.evalRPN(['4', '13', '5', '/', '+'])
+    assert 22 == solution.evalRPN([
+        '10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])
