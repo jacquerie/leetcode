cda528cbbe10c7a0560c61ee5b9e23e313f5988f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-16 19:16:50 +0200

    leetcode: add solution to problem 199

---
+++ leetcode/199_binary_tree_right_side_view.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        if root is None:
+            return []
+
+        result = []
+
+        current = [root]
+        while current:
+            result.append(current[-1].val)
+            next = []
+            for node in current:
+                if node.left is not None:
+                    next.append(node.left)
+                if node.right is not None:
+                    next.append(node.right)
+            current = next
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(1)
+    t0_1 = TreeNode(2)
+    t0_2 = TreeNode(3)
+    t0_3 = TreeNode(5)
+    t0_4 = TreeNode(4)
+    t0_2.right = t0_4
+    t0_1.right = t0_3
+    t0_0.left = t0_1
+    t0_0.right = t0_2
+
+    assert [1, 3, 4] == solution.rightSideView(t0_0)
