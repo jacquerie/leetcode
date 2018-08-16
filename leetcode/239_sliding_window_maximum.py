0bf62e197e3c4d192047682e3dd4a04bc09c21db
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-16 12:52:44 +0200

    leetcode: add solution to problem 239

---
+++ leetcode/239_sliding_window_maximum.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        result = []
+        seen = deque()
+
+        for i, num in enumerate(nums):
+            while seen and num >= nums[seen[-1]]:
+                seen.pop()
+            seen.append(i)
+            if i >= k and seen and seen[0] == i - k:
+                seen.popleft()
+            if i >= k - 1:
+                result.append(nums[seen[0]])
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [3, 3, 5, 5, 6, 7] == solution.maxSlidingWindow([
+        1, 3, -1, -3, 5, 3, 6, 7], 3)
