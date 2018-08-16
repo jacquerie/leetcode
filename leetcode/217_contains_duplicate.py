f19fadec2137fe1c82d3d640b8032d5bd2a4cbb8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-16 19:26:38 +0200

    leetcode: add solution to problem 217

---
+++ leetcode/217_contains_duplicate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.containsDuplicate([1, 2, 3, 1])
+    assert not solution.containsDuplicate([1, 2, 3, 4])
+    assert solution.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
