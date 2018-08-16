15bb74577ee62c79503896b18f4565937ff1a2ff
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-16 15:46:58 +0200

    leetcode: add solution to problem 174

---
+++ leetcode/174_dungeon_game.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        result = [float('inf') for _ in xrange(len(dungeon[0]))]
+        result[-1] = 1
+
+        for i in reversed(xrange(len(dungeon))):
+            result[-1] = max(result[-1] - dungeon[i][-1], 1)
+            for j in reversed(xrange(len(dungeon[0]) - 1)):
+                result[j] = max(min(result[j], result[j + 1]) - dungeon[i][j], 1)
+
+        return result[0]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 7 == solution.calculateMinimumHP([
+        [-2, -3, 3],
+        [-5, -10, 1],
+        [10, 30, -5],
+    ])
