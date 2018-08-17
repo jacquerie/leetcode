3bcc8905d9c16591a21a1b214fbf389ac9f7ec7d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-17 12:20:09 +0200

    leetcode: add solution to problem 862

---
+++ leetcode/862_shortest_subarray_with_sum_at_least_k.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def shortestSubarray(self, A, K):
+        prefix_sums = [0] * (len(A) + 1)
+        for i, el in enumerate(A):
+            prefix_sums[i + 1] = prefix_sums[i] + A[i]
+
+        result = float('inf')
+        seen = deque()
+
+        for i, prefix_sum in enumerate(prefix_sums):
+            while seen and prefix_sum <= prefix_sums[seen[-1]]:
+                seen.pop()
+            while seen and prefix_sum - prefix_sums[seen[0]] >= K:
+                result = min(result, i - seen.popleft())
+            seen.append(i)
+
+        return result if result != float('inf') else -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.shortestSubarray([1], 1)
+    assert -1 == solution.shortestSubarray([1, 2], 4)
+    assert 3 == solution.shortestSubarray([2, -1, 2], 3)
