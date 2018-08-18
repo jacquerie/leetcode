5e54b8774cae439ba982f3c0f8b60dd31228f973
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-18 19:01:17 +0200

    leetcode: add solution to problem 671

---
+++ leetcode/671_second_minimum_node_in_a_binary_tree.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def findSecondMinimumValue(self, root):
+        def _findSecondMinimumValue(root):
+            if root is None:
+                return [float('inf'), float('inf')]
+
+            leftMins = _findSecondMinimumValue(root.left)
+            rightMins = _findSecondMinimumValue(root.right)
+            mins = sorted(set(leftMins + [root.val] + rightMins))
+
+            return mins[:2]
+
+        _, result = _findSecondMinimumValue(root)
+        return result if result != float('inf') else -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(2)
+    t0_1 = TreeNode(2)
+    t0_2 = TreeNode(5)
+    t0_3 = TreeNode(5)
+    t0_4 = TreeNode(7)
+    t0_2.right = t0_4
+    t0_2.left = t0_3
+    t0_0.right = t0_1
+    t0_0.left = t0_2
+
+    assert 5 == solution.findSecondMinimumValue(t0_0)
+
+    t1_0 = TreeNode(2)
+    t1_1 = TreeNode(2)
+    t1_2 = TreeNode(2)
+    t1_0.right = t1_2
+    t1_0.left = t1_1
+
+    assert -1 == solution.findSecondMinimumValue(t1_0)
