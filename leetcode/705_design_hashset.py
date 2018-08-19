973321652ff83588caa5abe3b10415ee7495dcbe
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-20 00:59:16 +0200

    leetcode: add solution to problem 705

---
+++ leetcode/705_design_hashset.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class MyHashSet(object):
+    def __init__(self):
+        self.data = [False] * (2 ** 20)
+
+    def add(self, key):
+        self.data[key] = True
+
+    def contains(self, key):
+        return self.data[key]
+
+    def remove(self, key):
+        self.data[key] = False
+
+
+if __name__ == '__main__':
+    obj = MyHashSet()
+
+    obj.add(1)
+    obj.add(2)
+    assert obj.contains(1)
+    assert not obj.contains(3)
+    obj.add(2)
+    assert obj.contains(2)
+    obj.remove(2)
+    assert not obj.contains(2)
