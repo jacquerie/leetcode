011439f9f2d0548fb9228366dbbf2dab2c103d1c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-19 23:27:12 +0200

    leetcode: add solution to problem 890

---
+++ leetcode/890_find_and_replace_pattern.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        result = []
+
+        for word in words:
+            if self.wordMatchesPattern(word, pattern):
+                result.append(word)
+
+        return result
+
+    def wordMatchesPattern(self, word, pattern):
+        if len(word) != len(pattern):
+            return False
+
+        seen_w, seen_p = {}, {}
+        for w, p in zip(word, pattern):
+            if w in seen_w and seen_w[w] != p:
+                return False
+            if p in seen_p and seen_p[p] != w:
+                return False
+            seen_w[w] = p
+            seen_p[p] = w
+
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert ['mee', 'aqq'] == solution.findAndReplacePattern([
+        'abc', 'deq', 'mee', 'aqq', 'dkd', 'ccc'], 'abb')
