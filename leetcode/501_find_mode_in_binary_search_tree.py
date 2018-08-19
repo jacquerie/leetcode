f6a53295fff7a61d392216ed13c81730b7d1263a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-19 22:14:32 +0200

    leetcode: add solution to problem 501

---
+++ leetcode/501_find_mode_in_binary_search_tree.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def findMode(self, root):
+        def _findMode(current, previous, count, max_count, result):
+            if current is None:
+                return previous, count, max_count
+
+            previous, count, max_count = _findMode(
+                current.left, previous, count, max_count, result)
+
+            if previous is not None:
+                if previous.val == current.val:
+                    count += 1
+                else:
+                    count = 1
+            if count > max_count:
+                max_count = count
+                result[:] = []
+                result.append(current.val)
+            elif count == max_count:
+                result.append(current.val)
+
+            return _findMode(current.right, current, count, max_count, result)
+
+        result = []
+        if root is None:
+            return result
+
+        _findMode(root, None, 1, 0, result)
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(1)
+    t0_1 = TreeNode(2)
+    t0_2 = TreeNode(2)
+    t0_1.left = t0_2
+    t0_0.right = t0_1
+
+    assert [2] == solution.findMode(t0_0)
