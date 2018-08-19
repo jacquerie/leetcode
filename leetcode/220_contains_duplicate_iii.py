e3e49c2e38f18d195e68838dde40dbbb2edd4254
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-19 23:12:08 +0200

    leetcode: add solution to problem 220

---
+++ leetcode/220_contains_duplicate_iii.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import OrderedDict
+
+
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        buckets = OrderedDict()
+        for num in nums:
+            if len(buckets) > k:
+                buckets.popitem(last=False)
+            i = num if t == 0 else num // t
+            for j in (i - 1, i, i + 1):
+                if buckets.get(j) is not None and abs(num - buckets[j]) <= t:
+                    return True
+            buckets[i] = num
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0)
+    assert solution.containsNearbyAlmostDuplicate([1, 0, 1, 1], 1, 2)
+    assert not solution.containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3)
