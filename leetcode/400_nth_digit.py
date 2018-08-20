0f3d9cb0dd896f8972c44e8023c5859934f3716d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-20 19:56:39 +0200

    leetcode: add solution to problem 400

---
+++ leetcode/400_nth_digit.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def findNthDigit(self, n):
+        digits = 1
+        while n > 9 * digits * (10 ** (digits - 1)):
+            n -= 9 * digits * (10 ** (digits - 1))
+            digits += 1
+
+        num = str(10 ** (digits - 1) + (n - 1) // digits)
+        digit = (n - 1) % digits
+
+        return int(num[digit])
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.findNthDigit(3)
+    assert 0 == solution.findNthDigit(11)
+    assert 9 == solution.findNthDigit(9)
+    assert 3 == solution.findNthDigit(1000)
