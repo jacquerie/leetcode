9f89aa0c51c3d1f472bc930c3d583e6d8d829943
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-20 16:03:43 +0200

    leetcode: add solution to problem 357

---
+++ leetcode/357_count_numbers_with_unique_digits.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        if n > 10:
+            return 0
+        elif 1 <= n <= 10:
+            return 9 * self.factorialPrefix(9, n - 1) + self.countNumbersWithUniqueDigits(n - 1)
+        return 1
+
+    def factorialPrefix(self, m, n):
+        result = 1
+        while n:
+            result *= m
+            m -= 1
+            n -= 1
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 91 == solution.countNumbersWithUniqueDigits(2)
