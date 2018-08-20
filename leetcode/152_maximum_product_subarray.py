1302f1a1e7c95e61dd304661d0a426d8daa85ef0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-21 00:55:40 +0200

    leetcode: add solution to problem 152

---
+++ leetcode/152_maximum_product_subarray.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        best_max, current_max, current_min = float('-inf'), 1, 1
+        for num in nums:
+            current_max, current_min = max(current_min * num, num, current_max * num),\
+                min(current_min * num, num, current_max * num)
+            best_max = max(best_max, current_max)
+        return best_max
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.maxProduct([2, 3, -2, 4])
+    assert 0 == solution.maxProduct([-2, 0, -1])
