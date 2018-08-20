8a1f12948b30327e494dfa34678a19eaca711564
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-20 17:30:13 +0200

    leetcode: add solution to problem 643

---
+++ leetcode/643_maximum_average_subarray_i.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        current_sum = sum(nums[:k])
+        current_max = current_sum
+        for i, num in enumerate(nums[k:], k):
+            current_sum += nums[i] - nums[i - k]
+            current_max = max(current_max, current_sum)
+        return current_max / float(k)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 12.75 == solution.findMaxAverage([1, 12, -5, -6, 50, 3], 4)
