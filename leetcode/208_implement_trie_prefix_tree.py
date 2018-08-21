d9bcbc9901ae39c4e20ea40369b72bd860e28bee
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-21 03:09:00 +0200

    leetcode: add solution to problem 208

---
+++ leetcode/208_implement_trie_prefix_tree.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.leaf = False
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.leaf = True
+
+    def search(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.leaf
+
+    def startsWith(self, prefix):
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return True
+
+
+if __name__ == '__main__':
+    obj = Trie()
+
+    obj.insert('apple')
+    assert obj.search('apple')
+    assert not obj.search('app')
+    assert obj.startsWith('app')
+    obj.insert('app')
+    assert obj.search('app')
