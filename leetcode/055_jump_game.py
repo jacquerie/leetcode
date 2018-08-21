198d27c15e6b6e8cf9f32351df1f5c650cacc881
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-21 16:14:36 +0200

    leetcode: add solution to problem 055

---
+++ leetcode/055_jump_game.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def canJump(self, nums):
+        last = 0
+        for i, num in enumerate(nums):
+            if i > last:
+                break
+            last = max(last, i + num)
+        return last >= len(nums) - 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.canJump([2, 3, 1, 1, 4])
+    assert not solution.canJump([3, 2, 1, 0, 4])
