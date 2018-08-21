0b7f97ef688c200a44183c217dc5de3c94fd8b78
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-21 22:46:28 +0200

    leetcode: add solution to problem 401

---
+++ leetcode/401_binary_watch.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def readBinaryWatch(self, num):
+        result = []
+        for combination in itertools.combinations(range(10), num):
+            time = self.binaryToTime(self.combinationToBinary(combination))
+            if time is not None:
+                result.append(time)
+        return result
+
+    def combinationToBinary(self, combination):
+        result = ['0'] * 10
+        for i in combination:
+            result[i] = '1'
+        return result
+
+    def binaryToTime(self, binary):
+        hour = int(''.join(binary[:4]), 2)
+        minutes = int(''.join(binary[4:]), 2)
+        if 0 <= hour < 12 and 0 <= minutes < 60:
+            return '%d:%02d' % (hour, minutes)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [
+        '8:00',
+        '4:00',
+        '2:00',
+        '1:00',
+        '0:32',
+        '0:16',
+        '0:08',
+        '0:04',
+        '0:02',
+        '0:01',
+    ] == solution.readBinaryWatch(1)
