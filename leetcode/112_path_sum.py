b9f08c4d0599cb0b5179eebe43eea0dfb622ad45
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-22 09:15:37 +0200

    leetcode: add solution to problem 112

---
+++ leetcode/112_path_sum.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        if root is None:
+            return False
+        return self._hasPathSum(root, sum)
+
+    def _hasPathSum(self, root, sum):
+        if root.left is None and root.right is None:
+            return sum == root.val
+        elif root.left is None:
+            return self._hasPathSum(root.right, sum - root.val)
+        elif root.right is None:
+            return self._hasPathSum(root.left, sum - root.val)
+        return (
+            self._hasPathSum(root.left, sum - root.val) or
+            self._hasPathSum(root.right, sum - root.val)
+        )
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(5)
+    t0_1 = TreeNode(4)
+    t0_2 = TreeNode(8)
+    t0_3 = TreeNode(11)
+    t0_4 = TreeNode(13)
+    t0_5 = TreeNode(4)
+    t0_6 = TreeNode(7)
+    t0_7 = TreeNode(2)
+    t0_8 = TreeNode(1)
+    t0_5.right = t0_8
+    t0_3.right = t0_7
+    t0_3.left = t0_6
+    t0_2.right = t0_5
+    t0_2.left = t0_4
+    t0_1.left = t0_3
+    t0_0.right = t0_2
+    t0_0.left = t0_1
+
+    assert solution.hasPathSum(t0_0, 22)
