3f9cb34d9c4d3a22074479f6cf40e425f5fa8876
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-23 18:50:07 +0200

    leetcode: improve solution to problem 347

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -3,14 +3,43 @@
 from __future__ import absolute_import, division, print_function
 
 from collections import Counter
+from heapq import heappop, heappush
+
+
+class MaxHeap(object):
+    def __init__(self):
+        self.els = []
+
+    def __len__(self):
+        return len(self.els)
+
+    def __nonzero__(self):
+        return len(self.els) > 0
+
+    def pop(self):
+        _, el = heappop(self.els)
+        return el
+
+    def push(self, el, count):
+        heappush(self.els, (count, el))
 
 
 class Solution(object):
     def topKFrequent(self, nums, k):
-        return [el for el, _ in Counter(nums).most_common(k)]
+        counter, heap = Counter(nums), MaxHeap()
+        for el, count in counter.iteritems():
+            heap.push(el, count)
+            if len(heap) > k:
+                heap.pop()
+
+        result = []
+        while heap:
+            result.append(heap.pop())
+        return result[::-1]
 
 
 if __name__ == '__main__':
     solution = Solution()
 
     assert [1, 2] == solution.topKFrequent([1, 1, 1, 2, 2, 3], 2)
+    assert [1] == solution.topKFrequent([1], 1)
