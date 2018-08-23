dee284a3e6dba28ee2532b59d0bed09805ac2a0a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-23 21:40:51 +0200

    leetcode: add solution to problem 050

---
+++ leetcode/050_pow_x_n.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+def almost_equal(x, y, rel_tol=1e-09, abs_tol=0.0):
+    return abs(x - y) <= max(rel_tol * max(abs(x), abs(y)), abs_tol)
+
+
+class Solution(object):
+    def myPow(self, x, n):
+        positive = True
+        if n < 0:
+            n, positive = -n, False
+
+        result = 1.0
+        while n:
+            if n & 1:
+                result *= x
+            n >>= 1
+            x *= x
+
+        return result if positive else 1.0 / result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert almost_equal(1024.0, solution.myPow(2.0, 10))
+    assert almost_equal(9.261, solution.myPow(2.1, 3))
+    assert almost_equal(0.25, solution.myPow(2.0, -2))
