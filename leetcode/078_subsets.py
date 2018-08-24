a33943209f3190c01663ebb042a5be69e6bfb353
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-24 18:02:31 +0200

    leetcode: improve solution to problem 078

--- leetcode/078_subsets.py
+++ leetcode/078_subsets.py
@@ -2,13 +2,20 @@
 
 from __future__ import absolute_import, division, print_function
 
-import itertools
-
 
 class Solution(object):
     def subsets(self, nums):
-        return list(itertools.chain.from_iterable(
-            (list(el) for el in itertools.combinations(nums, n)) for n in xrange(len(nums) + 1)))
+        result = []
+        self._subsets(nums, [], result)
+        return result
+
+    def _subsets(self, nums, current, result):
+        if not nums:
+            if current not in result:
+                result.append(current)
+        else:
+            self._subsets(nums[1:], current, result)
+            self._subsets(nums[1:], current + [nums[0]], result)
 
 
 if __name__ == '__main__':
@@ -16,11 +23,11 @@ if __name__ == '__main__':
 
     assert [
         [],
-        [1],
-        [2],
         [3],
-        [1, 2],
-        [1, 3],
+        [2],
         [2, 3],
+        [1],
+        [1, 3],
+        [1, 2],
         [1, 2, 3],
     ] == solution.subsets([1, 2, 3])
