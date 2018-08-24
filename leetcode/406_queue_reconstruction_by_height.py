6ac0e905ff45f70d93a72cac954bbce34664da03
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-24 03:28:36 +0200

    leetcode: add solution to problem 406

---
+++ leetcode/406_queue_reconstruction_by_height.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def reconstructQueue(self, people):
+        people.sort(key=lambda person: (-person[0], person[1]))
+
+        result = []
+        for person in people:
+            result.insert(person[1], person)
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [
+        [5, 0],
+        [7, 0],
+        [5, 2],
+        [6, 1],
+        [4, 4],
+        [7, 1],
+    ] == solution.reconstructQueue([
+        [7, 0],
+        [4, 4],
+        [7, 1],
+        [5, 0],
+        [6, 1],
+        [5, 2],
+    ])
