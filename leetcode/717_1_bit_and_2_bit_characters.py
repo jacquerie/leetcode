0cf5be54a280dcbf2cfd921411ce9d3d46df3579
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-25 17:33:11 +0200

    leetcode: add solution to problem 717

---
+++ leetcode/717_1_bit_and_2_bit_characters.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        return self._isOneBitCharacter(bits, 0, len(bits))
+
+    def _isOneBitCharacter(self, bits, i, l):
+        if i == l:
+            return False
+        elif i == l - 1:
+            return True
+        elif bits[i] == 1:
+            return self._isOneBitCharacter(bits, i + 2, l)
+        return self._isOneBitCharacter(bits, i + 1, l)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isOneBitCharacter([1, 0, 0])
+    assert not solution.isOneBitCharacter([1, 1, 1, 0])
+    assert not solution.isOneBitCharacter([])
