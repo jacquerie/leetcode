86936d7235cdd49e5cb66535c34331b71a49fe5e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-30 01:12:10 +0200

    leetcode: add solution to problem 856

---
+++ leetcode/856_score_of_parentheses.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def scoreOfParentheses(self, S):
+        current_depth, result = 0, 0
+
+        for i, c in enumerate(S):
+            if c == '(':
+                current_depth += 1
+            else:
+                current_depth -= 1
+                if S[i - 1] == '(':
+                    result += 2 ** current_depth
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.scoreOfParentheses('()')
+    assert 2 == solution.scoreOfParentheses('(())')
+    assert 2 == solution.scoreOfParentheses('()()')
+    assert 6 == solution.scoreOfParentheses('(()(()))')
