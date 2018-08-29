d1adfe5c20cc19da7288aae17968b0cccf42aebe
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-29 17:48:54 +0200

    leetcode: add solution to problem 058

---
+++ leetcode/058_length_of_last_word.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        j = len(s) - 1
+        while j >= 0 and s[j] == ' ':
+            j -= 1
+
+        i = j
+        while i >= 0 and s[i] != ' ':
+            i -= 1
+
+        return j - i
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.lengthOfLastWord('Hello World')
+    assert 1 == solution.lengthOfLastWord('a')
+    assert 1 == solution.lengthOfLastWord('a ')
