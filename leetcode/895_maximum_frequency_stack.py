9323bc65e1821bd9584bcfe59a4d14f5687e1cc9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-29 17:04:53 +0200

    leetcode: add solution to problem 895

---
+++ leetcode/895_maximum_frequency_stack.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter, defaultdict
+
+
+class FreqStack(object):
+    def __init__(self):
+        self.counter = Counter()
+        self.elements = defaultdict(list)
+        self.max_count = 0
+
+    def push(self, x):
+        self.counter[x] += 1
+        if self.counter[x] > self.max_count:
+            self.max_count += 1
+        self.elements[self.counter[x]].append(x)
+
+    def pop(self):
+        x = self.elements[self.max_count].pop()
+        self.counter[x] -= 1
+        if not self.elements[self.max_count]:
+            self.max_count -= 1
+        return x
+
+
+if __name__ == '__main__':
+    obj = FreqStack()
+
+    obj.push(5)
+    obj.push(7)
+    obj.push(5)
+    obj.push(7)
+    obj.push(4)
+    obj.push(5)
+    assert 5 == obj.pop()
+    assert 7 == obj.pop()
+    assert 5 == obj.pop()
+    assert 4 == obj.pop()
