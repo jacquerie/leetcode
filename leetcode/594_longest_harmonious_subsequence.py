d318024f926e29653dcf0b79fe7847b802c1da7a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-30 03:15:28 +0200

    leetcode: add solution to problem 594

---
+++ leetcode/594_longest_harmonious_subsequence.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def findLHS(self, nums):
+        result = 0
+
+        counter = Counter(nums)
+        for num in counter:
+            if num + 1 in counter:
+                result = max(result, counter[num] + counter[num + 1])
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.findLHS([1, 3, 2, 2, 5, 2, 3, 7])
+    assert 0 == solution.findLHS([1, 1, 1, 1])
