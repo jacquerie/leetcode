1e32a21316ba9cb7be93a96cbf9adf8e10260e7d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-08-31 16:56:16 +0200

    leetcode: add solution to problem 515

---
+++ leetcode/515_find_largest_value_in_each_tree_row.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def largestValues(self, root):
+        result = []
+        if root is None:
+            return result
+
+        current = [root]
+        while current:
+            result.append(max(node.val for node in current))
+            next = []
+            for node in current:
+                if node.left is not None:
+                    next.append(node.left)
+                if node.right is not None:
+                    next.append(node.right)
+            current = next
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    t0_0 = TreeNode(1)
+    t0_1 = TreeNode(3)
+    t0_2 = TreeNode(2)
+    t0_3 = TreeNode(5)
+    t0_4 = TreeNode(3)
+    t0_5 = TreeNode(9)
+    t0_2.right = t0_5
+    t0_1.right = t0_4
+    t0_1.left = t0_3
+    t0_0.right = t0_2
+    t0_0.left = t0_1
+
+    assert [1, 3, 9] == solution.largestValues(t0_0)
