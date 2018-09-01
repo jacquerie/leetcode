ae5fdee035174b0440d343af448dd20a5f6ce5e2
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-01 19:01:21 +0200

    leetcode: add solution to problem 345

---
+++ leetcode/345_reverse_vowels_of_a_string.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+
+    VOWELS = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'}
+
+    def reverseVowels(self, s):
+        result = list(s)
+
+        i, j = 0, len(s) - 1
+        while i <= j:
+            if result[i] not in self.VOWELS:
+                i += 1
+            elif result[j] not in self.VOWELS:
+                j -= 1
+            else:
+                result[i], result[j] = result[j], result[i]
+                i += 1
+                j -= 1
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'holle' == solution.reverseVowels('hello')
+    assert 'Aa' == solution.reverseVowels('aA')
+    assert 'EO' == solution.reverseVowels('OE')
