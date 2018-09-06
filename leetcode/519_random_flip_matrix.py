b6ae74b8bb60a09092127efe14080562865ea129
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-07 00:22:04 +0200

    leetcode: add solution to problem 519

---
+++ leetcode/519_random_flip_matrix.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import random
+
+
+class Solution(object):
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.reset()
+
+    def flip(self):
+        self.count -= 1
+        index = random.randint(0, self.count)
+        result = self.elements.get(index, index)
+        self.elements[index] = self.elements.get(self.count, self.count)
+        return divmod(result, self.n_cols)
+
+    def reset(self):
+        self.count = self.n_rows * self.n_cols
+        self.elements = {}
+
+
+if __name__ == '__main__':
+    obj = Solution(2, 3)
+
+    assert obj.flip() in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+    assert obj.flip() in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+    assert obj.flip() in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+    assert obj.flip() in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+
+    obj = Solution(1, 2)
+
+    assert obj.flip() in [(0, 0), (0, 1)]
+    assert obj.flip() in [(0, 0), (0, 1)]
+    obj.reset()
+    assert obj.flip() in [(0, 0), (0, 1)]
