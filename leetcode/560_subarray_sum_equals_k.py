3874ea6504bcea505b01b795858334868bbed0a8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-08 22:10:59 +0200

    leetcode: add solution to problem 560

---
+++ leetcode/560_subarray_sum_equals_k.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        result = 0
+
+        cumsum, counter = 0, Counter([0])
+        for num in nums:
+            cumsum += num
+            result += counter[cumsum - k]
+            counter[cumsum] += 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.subarraySum([1, 1, 1], 2)
