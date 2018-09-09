aa775d681b7dabcc5b2c57a4e02f0dff344b03fb
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-09 02:22:26 +0200

    leetcode: add solution to problem 022

---
+++ leetcode/022_generate_parentheses.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+        self._generateParenthesis(n, 0, 0, '', result)
+        return result
+
+    def _generateParenthesis(self, n, i, p, current, result):
+        if i == 2 * n:
+            if p == 0:
+                result.append(current)
+            return
+        elif p < 0 or p > n:
+            return
+
+        self._generateParenthesis(n, i + 1, p + 1, current + '(', result)
+        self._generateParenthesis(n, i + 1, p - 1, current + ')', result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [
+        '((()))',
+        '(()())',
+        '(())()',
+        '()(())',
+        '()()()',
+    ] == solution.generateParenthesis(3)
