a648161da44c54191e5e472eb9886a48107813a9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-09 13:29:14 +0200

    leetcode: add solution to problem 397

---
+++ leetcode/397_integer_replacement.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def integerReplacement(self, n):
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        elif n == 3:
+            return 2
+        elif n % 4 == 1:
+            return 3 + self.integerReplacement((n - 1) // 4)
+        elif n % 4 == 3:
+            return 3 + self.integerReplacement((n + 1) // 4)
+        return 1 + self.integerReplacement(n // 2)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.integerReplacement(8)
+    assert 4 == solution.integerReplacement(7)
