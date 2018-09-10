2c33737ca0419614d39ea4d5ca9dadab24c7f487
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-10 21:55:41 +0200

    leetcode: add solution to problem 413

---
+++ leetcode/413_arithmetic_slices.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        result = 0
+        if len(A) < 3:
+            return result
+
+        first, step = 0, A[1] - A[0]
+        for i in xrange(2, len(A)):
+            if A[i] - A[i - 1] == step:
+                result += i - first - 1
+            else:
+                first, step = i - 1, A[i] - A[i - 1]
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.numberOfArithmeticSlices([1, 2, 3, 4])
+    assert 2 == solution.numberOfArithmeticSlices([1, 2, 3, 5, 8, 9, 10])
