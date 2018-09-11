710f8a48c3b256d64b7874206bfce869401cb90c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-11 09:06:00 +0200

    leetcode: add solution to problem 605

---
+++ leetcode/605_can_place_flowers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        for i in xrange(len(flowerbed)):
+            previous = i == 0 or flowerbed[i - 1] == 0
+            current = flowerbed[i] == 0
+            next = i == len(flowerbed) - 1 or flowerbed[i + 1] == 0
+
+            if previous and current and next:
+                flowerbed[i] = 1
+                n -= 1
+
+            if n <= 0:
+                return True
+
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.canPlaceFlowers([1, 0, 0, 0, 1], 1)
+    assert not solution.canPlaceFlowers([1, 0, 0, 0, 1], 2)
+    assert not solution.canPlaceFlowers([1, 0, 0, 0, 0, 1], 2)
+    assert solution.canPlaceFlowers([0, 0, 0, 0, 0, 1, 0, 0], 0)
