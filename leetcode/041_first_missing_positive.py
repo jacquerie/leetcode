dcf31e6b82f1bfe9525db97b8cf502a9868b4207
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-13 04:29:10 +0200

    leetcode: add solution to problem 041

---
+++ leetcode/041_first_missing_positive.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        i = 0
+        while i < len(nums):
+            if 0 <= nums[i] - 1 < len(nums) and nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+            else:
+                i += 1
+
+        for i, num in enumerate(nums, 1):
+            if i != num:
+                return i
+        return len(nums) + 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.firstMissingPositive([1, 2, 0])
+    assert 2 == solution.firstMissingPositive([3, 4, -1, 1])
+    assert 1 == solution.firstMissingPositive([7, 8, 9, 11, 12])
