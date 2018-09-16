329a5bee16fd5e53dbfcca6c2615e250818612be
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-16 10:21:32 +0200

    leetcode: add solution to problem 905

---
+++ leetcode/905_sort_array_by_parity.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        i, j = 0, len(A) - 1
+        while i < j:
+            if A[i] % 2 == 0:
+                i += 1
+            elif A[j] % 2 == 1:
+                j -= 1
+            elif A[i] % 2 == 1 and A[j] % 2 == 0:
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j -= 1
+
+        return A
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [4, 2, 1, 3] == solution.sortArrayByParity([3, 1, 2, 4])
