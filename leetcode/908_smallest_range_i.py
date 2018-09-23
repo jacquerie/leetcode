ad82133918c848d04060bcae96a19a4c3ee3ae22
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-23 22:31:25 +0200

    leetcode: add solution to problem 908

---
+++ leetcode/908_smallest_range_i.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        minimum, maximum = self.findMinMax(A)
+        return max((maximum - K) - (minimum + K), 0)
+
+    def findMinMax(self, A):
+        minimum, maximum = float('inf'), float('-inf')
+        for el in A:
+            minimum, maximum = min(minimum, el), max(maximum, el)
+        return minimum, maximum
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 0 == solution.smallestRangeI([1], 0)
+    assert 6 == solution.smallestRangeI([0, 10], 2)
+    assert 0 == solution.smallestRangeI([1, 3, 6], 3)
