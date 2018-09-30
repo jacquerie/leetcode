8c9980e5fc87c566256ef04e7ee3cebf7921e0d9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-09-30 17:52:10 +0200

    leetcode: add solution to problem 012

---
+++ leetcode/012_integer_to_roman.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+NUMERAL_ROMAN_MAP = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I'),
+]
+
+
+class Solution(object):
+    def intToRoman(self, num):
+        result = []
+
+        for numeral, roman in NUMERAL_ROMAN_MAP:
+            while num >= numeral:
+                result.append(roman)
+                num -= numeral
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'III' == solution.intToRoman(3)
+    assert 'IV' == solution.intToRoman(4)
+    assert 'IX' == solution.intToRoman(9)
+    assert 'LVIII' == solution.intToRoman(58)
+    assert 'MCMXCIV' == solution.intToRoman(1994)
