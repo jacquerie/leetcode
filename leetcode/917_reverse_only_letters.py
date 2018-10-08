62bf1129a7a92f620dda47b4aaddf68a6ab352b3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-08 20:13:27 +0200

    leetcode: add solution to problem 917

---
+++ leetcode/917_reverse_only_letters.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import string
+
+
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        result = list(S)
+
+        i, j = 0, len(S) - 1
+        while i <= j:
+            if result[i] not in string.ascii_letters:
+                i += 1
+            elif result[j] not in string.ascii_letters:
+                j -= 1
+            else:
+                result[i], result[j] = result[j], result[i]
+                i += 1
+                j -= 1
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'dc-ba' == solution.reverseOnlyLetters('ab-cd')
+    assert 'j-Ih-gfE-dCba' == solution.reverseOnlyLetters('a-bC-dEf-ghIj')
+    assert 'Qedo1ct-eeLg=ntse-T!' == solution.reverseOnlyLetters('Test1ng-Leet=code-Q!')
