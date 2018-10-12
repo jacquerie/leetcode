8097d7a40e30abdab495f1d02d825a0d62e4cf54
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-12 19:43:26 +0200

    leetcode: add solution to problem 470

---
+++ leetcode/470_implement_rand10_using_rand7.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import random
+
+random.seed(0)
+
+
+def rand7():
+    return random.randint(1, 7)
+
+
+class Solution(object):
+    def rand10(self):
+        x, y = 0, 0
+
+        while (not x or not y):
+            roll = rand7()
+            if 1 <= roll <= 2:
+                x = roll
+            elif 3 <= roll <= 7:
+                y = roll
+
+        return 5 * (x - 1) + (y - 2)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.rand10()
