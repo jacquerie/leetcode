bae1d1e21ccb244efbfba0549e5b5c6ae65fd3c1
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-16 23:03:55 +0200

    leetcode: add solution to problem 922

---
+++ leetcode/922_sort_array_by_parity_ii.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def sortArrayByParityII(self, A):
+        j = 1
+        for i in xrange(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+
+        return A
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [4, 5, 2, 7] == solution.sortArrayByParityII([4, 2, 5, 7])
