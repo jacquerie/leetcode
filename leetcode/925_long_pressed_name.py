7f7f2ae71157b998107c31b19c580942d26174fa
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-24 11:38:13 +0200

    leetcode: add solution to problem 925

---
+++ leetcode/925_long_pressed_name.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        for name_group, typed_group in itertools.izip_longest(
+                itertools.groupby(name), itertools.groupby(typed)):
+            if name_group is None or typed_group is None:
+                return False
+            elif name_group[0] != typed_group[0]:
+                return False
+            elif len(list(name_group[1])) > len(list(typed_group[1])):
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isLongPressedName('alex', 'aaleex')
+    assert not solution.isLongPressedName('saeed', 'ssaaedd')
+    assert solution.isLongPressedName('leelee', 'lleeelee')
+    assert solution.isLongPressedName('laiden', 'laiden')
