f33fb8f6524c42c2e0be2d615e910b79b71f14e9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-27 10:24:56 +0200

    leetcode: add solution to problem 914

---
+++ leetcode/914_x_of_a_kind_in_a_deck_of_cards.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def hasGroupsSizeX(self, deck):
+        counts = Counter(deck).values()
+
+        min_count = min(counts)
+        if min_count == 1:
+            return False
+
+        partition_size = min_count
+        for count in counts:
+            remainder = count % min_count
+            if remainder > 1:
+                partition_size = min(partition_size, remainder)
+
+        return all(count % partition_size == 0 for count in counts)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.hasGroupsSizeX([1, 2, 3, 4, 4, 3, 2, 1])
+    assert not solution.hasGroupsSizeX([1, 1, 1, 2, 2, 2, 3, 3])
+    assert not solution.hasGroupsSizeX([1])
+    assert solution.hasGroupsSizeX([1, 1])
+    assert solution.hasGroupsSizeX([1, 1, 2, 2, 2, 2])
+    assert solution.hasGroupsSizeX([1, 1, 1, 1, 2, 2, 2, 2, 2, 2])
