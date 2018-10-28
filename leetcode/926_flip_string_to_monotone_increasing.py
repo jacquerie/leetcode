0bd85cda2eb29a3049c844d9a9f7855c233da3ea
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-28 21:39:26 +0100

    leetcode: add solution to problem 926

---
+++ leetcode/926_flip_string_to_monotone_increasing.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def minFlipsMonoIncr(self, S):
+        ones_count, result = 0, 0
+
+        for char in S:
+            if char == '0' and ones_count == 0:
+                continue
+            elif char == '0':
+                result += 1
+            else:
+                ones_count += 1
+
+            if result > ones_count:
+                result = ones_count
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.minFlipsMonoIncr('00110')
+    assert 2 == solution.minFlipsMonoIncr('010110')
+    assert 2 == solution.minFlipsMonoIncr('00011000')
+    assert 3 == solution.minFlipsMonoIncr('0101100011')
