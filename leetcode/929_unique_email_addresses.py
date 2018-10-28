415761fbe797c6904a57e6d7d94e6a08650aebd6
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-28 15:36:40 +0100

    leetcode: add solution to problem 929

---
+++ leetcode/929_unique_email_addresses.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import namedtuple
+
+Email = namedtuple('Email', ['local_name', 'domain_name'])
+
+
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        result = set()
+        for email in emails:
+            raw_local_name, domain_name = email.split('@')
+            local_name = raw_local_name.replace('.', '').split('+')[0]
+            result.add(Email(local_name, domain_name))
+        return len(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.numUniqueEmails([
+        'test.email+alex@leetcode.com',
+        'test.e.mail+bob.cathy@leetcode.com',
+        'testemail+david@lee.tcode.com',
+    ])
