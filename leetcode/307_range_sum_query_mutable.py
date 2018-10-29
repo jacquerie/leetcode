90cfc478e21f3281c7710a3e1701cb35a45f9434
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-29 13:44:13 +0100

    leetcode: add solution to problem 307

---
+++ leetcode/307_range_sum_query_mutable.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class FenwickTree(object):
+    def __init__(self, nums):
+        self.els = [0] * (len(nums) + 2)
+        for i, num in enumerate(nums, 1):
+            self.add(i, num)
+
+    def add(self, i, k):
+        while i < len(self.els):
+            self.els[i] += k
+            i += i & -i
+
+    def sumPrefix(self, i):
+        result = 0
+        while i > 0:
+            result += self.els[i]
+            i -= i & -i
+        return result
+
+
+class NumArray(object):
+    def __init__(self, nums):
+        self.tree = FenwickTree(nums)
+
+    def update(self, i, val):
+        self.tree.add(i + 1, val - self.sumRange(i, i))
+
+    def sumRange(self, i, j):
+        return self.tree.sumPrefix(j + 1) - self.tree.sumPrefix(i)
+
+
+if __name__ == '__main__':
+    obj = NumArray([1, 3, 5])
+
+    assert 9 == obj.sumRange(0, 2)
+    obj.update(1, 2)
+    assert 8 == obj.sumRange(0, 2)
+
+    obj = NumArray([-1])
+
+    assert -1 == obj.sumRange(0, 0)
+    obj.update(0, 1)
+    assert 1 == obj.sumRange(0, 0)
