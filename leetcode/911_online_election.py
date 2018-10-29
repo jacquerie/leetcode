bea5ac074e3fa9d204d2a7d7df8e0fca9485e87d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-10-29 10:22:25 +0100

    leetcode: add solution to problem 911

---
+++ leetcode/911_online_election.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from bisect import bisect
+from collections import Counter
+
+
+class TopVotedCandidate(object):
+    def __init__(self, persons, times):
+        self.leaders = []
+        self.times = times
+
+        leader, votes = None, Counter()
+        for person in persons:
+            votes[person] += 1
+            if votes[person] >= votes[leader]:
+                leader = person
+            self.leaders.append(leader)
+
+    def q(self, t):
+        return self.leaders[bisect(self.times, t) - 1]
+
+
+if __name__ == '__main__':
+    obj = TopVotedCandidate([0, 1, 1, 0, 0, 1, 0], [0, 5, 10, 15, 20, 25, 30])
+
+    assert 0 == obj.q(3)
+    assert 1 == obj.q(12)
+    assert 1 == obj.q(25)
+    assert 0 == obj.q(15)
+    assert 0 == obj.q(24)
+    assert 1 == obj.q(8)
