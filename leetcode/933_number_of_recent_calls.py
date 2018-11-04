e91e85a25b5030310d3af7e757cbddc04887904c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-11-04 23:34:07 +0100

    leetcode: add solution to problem 933

---
+++ leetcode/933_number_of_recent_calls.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class RecentCounter(object):
+    def __init__(self):
+        self.count = 0
+        self.queue = deque()
+
+    def ping(self, t):
+        while self.queue and t - self.queue[0] > 3000:
+            self.count -= 1
+            self.queue.popleft()
+        self.count += 1
+        self.queue.append(t)
+        return self.count
+
+
+if __name__ == '__main__':
+    obj = RecentCounter()
+
+    assert 1 == obj.ping(1)
+    assert 2 == obj.ping(100)
+    assert 3 == obj.ping(3001)
+    assert 3 == obj.ping(3002)
