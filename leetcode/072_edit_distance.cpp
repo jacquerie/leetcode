470a87c448292cc4cccafb7c26d8941406a916a9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-11-30 23:12:06 +0100

    leetcode: improve c++ solution to problem 072

--- leetcode/072_edit_distance.cpp
+++ leetcode/072_edit_distance.cpp
@@ -13,21 +13,21 @@ class Solution {
         int len1 = word1.size(), len2 = word2.size();
         vector<vector<int>> distances(len1 + 1, vector<int>(len2 + 1));
 
-        distances[0][0] = 0;
-        for (int i = 1; i <= len1; i++) {
-            distances[i][0] = i;
-        }
-        for (int j = 1; j <= len2; j++) {
-            distances[0][j] = j;
-        }
-
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                distances[i][j] = min({
-                    distances[i - 1][j] + 1,
-                    distances[i][j - 1] + 1,
-                    distances[i - 1][j - 1] + (word1[i - 1] == word2[j - 1] ? 0 : 1),
-                });
+        for (int i = 0; i < len1 + 1; i++) {
+            for (int j = 0; j < len2 + 1; j++) {
+                if (i == 0 && j == 0) {
+                    distances[i][j] = 0;
+                } else if (i == 0) {
+                    distances[i][j] = j;
+                } else if (j == 0) {
+                    distances[i][j] = i;
+                } else {
+                    distances[i][j] = min({
+                        distances[i - 1][j] + 1,
+                        distances[i][j - 1] + 1,
+                        distances[i - 1][j - 1] + (word1[i - 1] == word2[j - 1] ? 0 : 1),
+                    });
+                }
             }
         }
 
