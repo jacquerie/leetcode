1e1e7f2257738762c2b743ded2bd3885f7ba936a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-13 13:07:32 +0100

    leetcode: add solution to problem 954

---
+++ leetcode/954_array_of_doubled_pairs.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def canReorderDoubled(self, A):
+        counts = Counter(A)
+        for el in sorted(counts, key=abs):
+            if counts[2 * el] < counts[el]:
+                return False
+            counts[2 * el] -= counts[el]
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert not solution.canReorderDoubled([3, 1, 3, 6])
+    assert not solution.canReorderDoubled([2, 1, 2, 6])
+    assert solution.canReorderDoubled([4, -2, 2, -4])
+    assert not solution.canReorderDoubled([1, 2, 4, 16, 8, 4])
