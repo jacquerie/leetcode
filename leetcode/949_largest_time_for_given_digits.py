74dc2abc871bd7329c672bd0e7ea911875bf6fbe
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-16 04:25:05 +0100

    leetcode: add solution to problem 949

---
+++ leetcode/949_largest_time_for_given_digits.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import itertools
+
+
+class Solution(object):
+    def largestTimeFromDigits(self, A):
+        for digits in itertools.permutations(sorted(A, reverse=True)):
+            if self.isValidTime(digits):
+                return '{}{}:{}{}'.format(*digits)
+        return ''
+
+    def isValidTime(self, digits):
+        hour = 10 * digits[0] + digits[1]
+        minute = 10 * digits[2] + digits[3]
+        return 0 <= hour <= 23 and 0 <= minute <= 59
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert '23:41' == solution.largestTimeFromDigits([1, 2, 3, 4])
+    assert '' == solution.largestTimeFromDigits([5, 5, 5, 5])
+    assert '00:00' == solution.largestTimeFromDigits([0, 0, 0, 0])
+    assert '10:00' == solution.largestTimeFromDigits([0, 0, 1, 0])
