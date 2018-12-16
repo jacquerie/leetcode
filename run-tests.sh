8c3a07629143a7f6b0201d516a2ddbb44be0deed
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-16 14:50:42 +0100

    travis: compile the c++ code as part of the build

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,5 @@ set -e
 
 cpplint --recursive --quiet leetcode
 flake8 leetcode
+ls -1 leetcode/*.cpp | xargs --max-args 1 --max-procs 2 g++ --std=c++11
 ls -1 leetcode/*.py | xargs --max-args 1 --max-procs 2 python
