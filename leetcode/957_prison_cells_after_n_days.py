e326db5c4a01cf378e6bfd9e02940b2a3fc60b2b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-17 22:36:34 +0100

    leetcode: add solution to problem 957

---
+++ leetcode/957_prison_cells_after_n_days.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def prisonAfterNDays(self, cells, N):
+        for i in xrange(N % 14 + 14):
+            cells = tuple(
+                1 if 0 < j < 7 and cells[j - 1] == cells[j + 1] else 0 for j in xrange(8))
+        return list(cells)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [0, 0, 1, 1, 0, 0, 0, 0] == solution.prisonAfterNDays([0, 1, 0, 1, 1, 0, 0, 1], 7)
+    assert [0, 0, 1, 1, 1, 1, 1, 0] == solution.prisonAfterNDays([1, 0, 0, 1, 0, 0, 1, 0], 1000000000)
+    assert [0, 0, 1, 0, 0, 1, 0, 0] == solution.prisonAfterNDays([1, 1, 0, 1, 1, 0, 1, 1], 6)
+    assert [0, 0, 0, 1, 1, 0, 1, 0] == solution.prisonAfterNDays([0, 0, 0, 1, 1, 0, 1, 0], 574)
+    assert [0, 1, 1, 0, 1, 1, 1, 0] == solution.prisonAfterNDays([1, 0, 0, 1, 0, 0, 0, 1], 826)
