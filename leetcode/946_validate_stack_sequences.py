3fd9f1f029b2731b760715b107aa0b7b0c818d96
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-18 16:05:16 +0100

    leetcode: add solution to problem 946

---
+++ leetcode/946_validate_stack_sequences.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        i, j, stack = 0, 0, deque()
+        while i < len(pushed) or j < len(popped):
+            if j < len(popped) and stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+            elif i < len(pushed):
+                stack.append(pushed[i])
+                i += 1
+            else:
+                return False
+        return i == len(pushed) and j == len(popped)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 2, 1])
+    assert not solution.validateStackSequences([1, 2, 3, 4, 5], [4, 3, 5, 1, 2])
