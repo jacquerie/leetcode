65439b95b08355a53c255fac475e41432de4978b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-21 01:18:33 +0100

    leetcode: add solution to problem 942

---
+++ leetcode/942_di_string_match.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def diStringMatch(self, S):
+        i, j, result = 0, 0, [0]
+
+        for c in S:
+            if c == 'I':
+                i += 1
+                result.append(i)
+            else:
+                j -= 1
+                result.append(j)
+
+        return [el - j for el in result]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [2, 3, 1, 4, 0] == solution.diStringMatch('IDID')
+    assert [0, 1, 2, 3] == solution.diStringMatch('III')
+    assert [2, 1, 0, 3] == solution.diStringMatch('DDI')
