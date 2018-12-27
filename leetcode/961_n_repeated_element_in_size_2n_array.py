fd1e1719a06f1f7c0f90c4bf97944d7ecf2acbbb
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-27 17:41:39 +0100

    leetcode: add solution to problem 961

---
+++ leetcode/961_n_repeated_element_in_size_2n_array.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def repeatedNTimes(self, A):
+        seen = set()
+        for el in A:
+            if el in seen:
+                return el
+            seen.add(el)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.repeatedNTimes([1, 2, 3, 3])
+    assert 2 == solution.repeatedNTimes([2, 1, 2, 5, 3, 2])
+    assert 5 == solution.repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])
