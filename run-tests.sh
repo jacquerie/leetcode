130d24092e5a2b1127a5727b8c66b995f236a403
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-12-28 03:07:47 +0100

    travis: run the c++ tests as part of the build

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,5 @@ set -e
 
 cpplint --recursive --quiet leetcode
 flake8 leetcode
-ls -1 leetcode/*.cpp | xargs --max-args 1 --max-procs 2 g++ --std=c++11 -Wall -Wextra -Werror -Wno-return-type -Wno-sign-compare -Wno-unused-but-set-variable
+ls -1 leetcode/*.cpp | xargs --max-args 1 --max-procs 1 -I % sh -c 'g++ --std=c++11 -Wall -Wextra -Werror -Wno-return-type -Wno-sign-compare -Wno-unused-but-set-variable %; ./a.out;'
 ls -1 leetcode/*.py | xargs --max-args 1 --max-procs 2 python
