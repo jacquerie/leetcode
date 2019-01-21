b7b57bad52a92e76d1ca7cefc3b214c24d145a84
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-21 17:51:20 +0100

    leetcode: add solution to problem 976

---
+++ leetcode/976_largest_perimeter_triangle.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def largestPerimeter(self, A):
+        A.sort(reverse=True)
+        for i in xrange(len(A) - 2):
+            if A[i + 2] + A[i + 1] > A[i]:
+                return A[i + 2] + A[i + 1] + A[i]
+        return 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.largestPerimeter([2, 1, 2])
+    assert 0 == solution.largestPerimeter([1, 2, 1])
+    assert 10 == solution.largestPerimeter([3, 2, 3, 4])
+    assert 8 == solution.largestPerimeter([3, 6, 2, 3])
