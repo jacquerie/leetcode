628a00f9d79f15ca30bd7eb1b4aa6ec85993ac09
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-21 17:22:45 +0100

    leetcode: add solution to problem 977

---
+++ leetcode/977_squares_of_a_sorted_array.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def sortedSquares(self, A):
+        result = []
+
+        i, j = 0, len(A) - 1
+        while i <= j:
+            if abs(A[i]) < abs(A[j]):
+                result.append(A[j] * A[j])
+                j -= 1
+            else:
+                result.append(A[i] * A[i])
+                i += 1
+
+        return result[::-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [0, 1, 9, 16, 100] == solution.sortedSquares([-4, -1, 0, 3, 10])
+    assert [4, 9, 9, 49, 121] == solution.sortedSquares([-7, -3, 2, 3, 11])
