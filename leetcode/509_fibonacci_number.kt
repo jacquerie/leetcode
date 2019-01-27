a3c2436d356034a7386596d00e4210d1e07fb9a4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-27 02:26:30 +0100

    leetcode: add kotlin solution to problem 509

---
+++ leetcode/509_fibonacci_number.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+class Solution {
+  fun fib(N: Int): Int {
+    var current = 0
+    var next = 1
+
+    repeat(N) {
+      val tmp = next
+      next = current + next
+      current = tmp
+    }
+
+    return current
+  }
+}
+
+fun main(args: Array<String>) {
+  val solution = Solution()
+
+  assert(1 == solution.fib(2))
+  assert(2 == solution.fib(3))
+  assert(3 == solution.fib(4))
+}
