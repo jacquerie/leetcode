70375fae9ced638ef7e661b7b0ee3ac2297c0f3b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-29 00:52:52 +0100

    travis: add ktlint

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,8 @@
 set -e
 
 cpplint --recursive --quiet leetcode
+ktlint leetcode/*.kt
 flake8 leetcode
+
 ls -1 leetcode/*.cpp | xargs --max-args 1 --max-procs 1 -I % sh -c 'g++ --std=c++11 -Wall -Wextra -Werror -Wno-return-type -Wno-sign-compare -Wno-unused-but-set-variable %; ./a.out;'
 ls -1 leetcode/*.py | xargs --max-args 1 --max-procs 2 python
