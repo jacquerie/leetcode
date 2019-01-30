85d70c7f4f6dd959c61595a6e7b0c1f7961ffe8c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-30 05:17:26 +0100

    leetcode: add solution to problem 984

---
+++ leetcode/984_string_without_aaa_or_bbb.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def strWithout3a3b(self, A, B):
+        if A >= 2 * B:
+            result = ['aab'] * B + ['a'] * (A - 2 * B)
+        elif A >= B:
+            result = ['aab'] * (A - B) + ['ab'] * (2 * B - A)
+        elif B >= 2 * A:
+            result = ['bba'] * A + ['b'] * (B - 2 * A)
+        else:
+            result = ['bba'] * (B - A) + ['ab'] * (2 * A - B)
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'bba' == solution.strWithout3a3b(1, 2)
+    assert 'aabaa' == solution.strWithout3a3b(4, 1)
