2cb7517e789ebd80eee34e89020f4e671822257b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-31 21:22:40 +0100

    leetcode: add kotlin solution to problem 917

---
+++ leetcode/917_reverse_only_letters.kt
@@ -0,0 +1,34 @@
+package leetcode
+
+class Solution {
+    fun reverseOnlyLetters(S: String): String {
+        var result = S.toCharArray()
+
+        var i = 0
+        var j = S.length - 1
+        while (i <= j) {
+            if (!result[i].isLetter()) {
+                i++
+            } else if (!result[j].isLetter()) {
+                j--
+            } else {
+                val tmp = result[j]
+                result[j] = result[i]
+                result[i] = tmp
+
+                i++
+                j--
+            }
+        }
+
+        return result.joinToString("")
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+
+    assert("dc-ba" == solution.reverseOnlyLetters("ab-cd"))
+    assert("j-Ih-gfE-dCba" == solution.reverseOnlyLetters("a-bC-dEf-ghIj"))
+    assert("Qedo1ct-eeLg=ntse-T!" == solution.reverseOnlyLetters("Test1ng-Leet=code-Q!"))
+}
