c71930ae64fb876ddac4925de09fa6dd8fab1046
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-02-03 19:31:11 +0100

    leetcode: add solution to problem 985

---
+++ leetcode/985_sum_of_even_numbers_after_queries.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def sumEvenAfterQueries(self, A, queries):
+        result = []
+
+        sumEven = 0
+        for el in A:
+            if el % 2 == 0:
+                sumEven += el
+
+        for val, index in queries:
+            A[index] += val
+            if A[index] % 2 == 0 and val % 2 == 0:
+                sumEven += val
+            elif A[index] % 2 == 0 and val % 2 == 1:
+                sumEven += A[index]
+            elif A[index] % 2 == 1 and val % 2 == 1:
+                sumEven -= A[index] - val
+            result.append(sumEven)
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [8, 6, 2, 4] == solution.sumEvenAfterQueries(
+        [1, 2, 3, 4], [[1, 0], [-3, 1], [-4, 0], [2, 3]])
