4ba36c56996d1c8ce54fe0213ea241dba7088231
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-02-05 16:20:12 +0100

    leetcode: improve c++ solution to problem 876

--- leetcode/876_middle_of_the_linked_list.cpp
+++ leetcode/876_middle_of_the_linked_list.cpp
@@ -12,14 +12,6 @@ struct ListNode {
     explicit ListNode(int x): val(x), next(NULL) {}
 };
 
-bool operator==(ListNode& lhs, ListNode& rhs) {
-    if (lhs.next && rhs.next) {
-        return lhs.val == rhs.val && *(lhs.next) == *(rhs.next);
-    } else {
-        return !(lhs.next || rhs.next);
-    }
-}
-
 class Solution {
  public:
     ListNode* middleNode(ListNode* head) {
@@ -47,31 +39,19 @@ int main() {
     l0_1.next = &l0_2;
     l0_0.next = &l0_1;
 
-    auto l1_0 = ListNode(3);
-    auto l1_1 = ListNode(4);
-    auto l1_2 = ListNode(5);
+    assert(&l0_2 == solution.middleNode(&l0_0));
+
+    auto l1_0 = ListNode(1);
+    auto l1_1 = ListNode(2);
+    auto l1_2 = ListNode(3);
+    auto l1_3 = ListNode(4);
+    auto l1_4 = ListNode(5);
+    auto l1_5 = ListNode(6);
+    l1_4.next = &l1_5;
+    l1_3.next = &l1_4;
+    l1_2.next = &l1_3;
     l1_1.next = &l1_2;
     l1_0.next = &l1_1;
 
-    assert(l1_0 == *(solution.middleNode(&l0_0)));
-
-    auto l2_0 = ListNode(1);
-    auto l2_1 = ListNode(2);
-    auto l2_2 = ListNode(3);
-    auto l2_3 = ListNode(4);
-    auto l2_4 = ListNode(5);
-    auto l2_5 = ListNode(6);
-    l2_4.next = &l2_5;
-    l2_3.next = &l2_4;
-    l2_2.next = &l2_3;
-    l2_1.next = &l2_2;
-    l2_0.next = &l2_1;
-
-    auto l3_0 = ListNode(4);
-    auto l3_1 = ListNode(5);
-    auto l3_2 = ListNode(6);
-    l3_1.next = &l3_2;
-    l3_0.next = &l3_1;
-
-    assert(l3_0 == *(solution.middleNode(&l2_0)));
+    assert(&l1_3 == solution.middleNode(&l1_0));
 }
