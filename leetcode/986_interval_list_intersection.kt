3fa625f94868d08bcd35d5e669e5146770f7122d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-02-06 05:46:47 +0100

    leetcode: add kotlin solution to problem 986

---
+++ leetcode/986_interval_list_intersection.kt
@@ -0,0 +1,65 @@
+package leetcode
+
+class Interval(var start: Int = 0, var end: Int = 0)
+
+class Solution {
+    fun intervalIntersection(A: Array<Interval>, B: Array<Interval>): Array<Interval> {
+        val C = mutableListOf<Interval>()
+
+        var i = 0
+        var j = 0
+        while (i < A.size && j < B.size) {
+            if (A[i].end < B[j].start) {
+                i++
+            } else if (B[j].end < A[i].start) {
+                j++
+            } else {
+                C.add(
+                    Interval(
+                        maxOf(A[i].start, B[j].start),
+                        minOf(A[i].end, B[j].end)
+                    )
+                )
+
+                if (A[i].end < B[j].end) {
+                    i++
+                } else {
+                    j++
+                }
+            }
+        }
+
+        val result = Array<Interval>(C.size, { _ -> Interval() })
+        C.forEachIndexed({ k, el -> result[k] = el })
+        return result
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+
+    val A = arrayOf(
+        Interval(0, 2),
+        Interval(5, 10),
+        Interval(13, 23),
+        Interval(24, 25)
+    )
+    val B = arrayOf(
+        Interval(1, 5),
+        Interval(8, 12),
+        Interval(15, 24),
+        Interval(25, 26)
+    )
+
+    val expected = arrayOf(
+        Interval(1, 2),
+        Interval(5, 5),
+        Interval(8, 10),
+        Interval(15, 23),
+        Interval(24, 24),
+        Interval(25, 25)
+    )
+    val result = solution.intervalIntersection(A, B)
+
+    assert(expected contentEquals result)
+}
