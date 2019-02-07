a57dd0fd72c8aa5e953bc20a41b9d2af3395a59a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-02-07 01:01:41 +0100

    leetcode: add solution to problem 986

---
+++ leetcode/986_interval_list_intersection.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+
+class Solution(object):
+    def intervalIntersection(self, A, B):
+        result = []
+
+        i, j = 0, 0
+        while i < len(A) and j < len(B):
+            if A[i].end < B[j].start:
+                i += 1
+            elif B[j].end < A[i].start:
+                j += 1
+            else:
+                result.append(
+                    Interval(
+                        max(A[i].start, B[j].start),
+                        min(A[i].end, B[j].end)
+                    )
+                )
+
+                if A[i].end < B[j].end:
+                    i += 1
+                else:
+                    j += 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    A = [
+        Interval(0, 2),
+        Interval(5, 10),
+        Interval(13, 23),
+        Interval(24, 25),
+    ]
+    B = [
+        Interval(1, 5),
+        Interval(8, 12),
+        Interval(15, 24),
+        Interval(25, 26),
+    ]
+
+    expected = [
+        Interval(1, 2),
+        Interval(5, 5),
+        Interval(8, 10),
+        Interval(15, 23),
+        Interval(24, 24),
+        Interval(25, 25),
+    ]
+    result = solution.intervalIntersection(A, B)
+
+    assert expected == result
