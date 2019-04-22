3e59bea53e290a64281cd56f52437a4c6052bbf7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-04-22 22:41:52 +0200

    leetcode: add solution to problem 0912

---
+++ leetcode/0912_sort_an_array.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def sortArray(self, nums):
+        return sorted(nums)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [1, 2, 3, 5] == solution.sortArray([5, 2, 3, 1])
+    assert [0, 0, 1, 1, 2, 5] == solution.sortArray([5, 1, 1, 2, 0, 0])
