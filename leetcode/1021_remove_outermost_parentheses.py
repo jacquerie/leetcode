9c9c65344db298925da7d0ed119c8f785af6d630
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-04-22 22:25:44 +0200

    leetcode: add solution to problem 1021

---
+++ leetcode/1021_remove_outermost_parentheses.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def removeOuterParentheses(self, S):
+        result = []
+
+        count = 0
+        for c in S:
+            if c == '(':
+                if count > 0:
+                    result.append(c)
+                count += 1
+            elif c == ')':
+                if count > 1:
+                    result.append(c)
+                count -= 1
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert '()()()' == solution.removeOuterParentheses('(()())(())')
+    assert '()()()()(())' == solution.removeOuterParentheses('(()())(())(()(()))')
+    assert '' == solution.removeOuterParentheses('()()')
