fd9f23300889ea0a0ef3761bb3f491a5767a16c2
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-09-07 15:50:27 +0200

    leetcode: add solution to problem 1047

---
+++ leetcode/1047_remove_all_adjacent_duplicates_in_a_string.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def removeDuplicates(self, S):
+        result = deque()
+
+        for char in S:
+            if result and char == result[-1]:
+                result.pop()
+            else:
+                result.append(char)
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'ca' == solution.removeDuplicates('abbaca')
