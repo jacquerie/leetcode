d5ba19e47b86444baf8a6f1f79dfa56491e340f9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-02 01:10:24 +0200

    leetcode: add solution to problem 1207

---
+++ leetcode/1207_unique_number_of_occurrences.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        counts = Counter(arr)
+        return self.allDistinct(counts.values())
+
+    def allDistinct(self, arr):
+        return len(arr) == len(set(arr))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.uniqueOccurrences([1, 2, 2, 1, 1, 3])
+    assert not solution.uniqueOccurrences([1, 2])
+    assert solution.uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0])
