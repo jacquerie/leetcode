a8f210eb9c596ac4b9e55dd8a87d71022e54a59a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-05 13:57:26 +0200

    leetcode: add solution to problem 0665

---
+++ leetcode/0665_non_decreasing_array.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def checkPossibility(self, nums):
+        peak = None
+        for i in xrange(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                if peak is not None:
+                    return False
+                peak = i
+
+        return (
+            peak is None or
+            peak == 0 or
+            peak == len(nums) - 2 or
+            nums[peak - 1] <= nums[peak + 1] or
+            nums[peak] <= nums[peak + 2]
+        )
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.checkPossibility([4, 2, 3])
+    assert not solution.checkPossibility([4, 2, 1])
+    assert solution.checkPossibility([1, 2, 4, 5, 3])
+    assert solution.checkPossibility([1, 1, 1])
+    assert not solution.checkPossibility([3, 4, 2, 3])
