7d91f31d29dd285d7c6dde853ee9d6e4ddd3ef9f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-07 09:00:11 +0200

    leetcode: add solution to problem 1030

---
+++ leetcode/1030_matrix_cells_in_distance_order.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import deque
+
+
+class Solution(object):
+    def allCellsDistOrder(self, R, C, r0, c0):
+        queue, result, visited = deque([(r0, c0)]), [], set()
+
+        while queue:
+            r, c = queue.popleft()
+            if (r, c) in visited:
+                continue
+
+            result.append([r, c])
+            visited.add((r, c))
+
+            if 0 < r:
+                queue.append((r - 1, c))
+            if 0 < c:
+                queue.append((r, c - 1))
+            if r < R - 1:
+                queue.append((r + 1, c))
+            if c < C - 1:
+                queue.append((r, c + 1))
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [[0, 0], [0, 1]] == solution.allCellsDistOrder(1, 2, 0, 0)
+    assert [[0, 1], [0, 0], [1, 1], [1, 0]] == solution.allCellsDistOrder(2, 2, 0, 1)
