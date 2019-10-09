42fe958c3a9f521119d40ce02548683c23cb8e10
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-09 06:40:16 +0200

    leetcode: add solution to problem 1137

---
+++ leetcode/1137_nth_tribonacci_number.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def tribonacci(self, n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        else:
+            x, y, z = 0, 1, 1
+            for _ in xrange(3, n + 1):
+                x, y, z = y, z, x + y + z
+            return z
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 4 == solution.tribonacci(4)
+    assert 1389537 == solution.tribonacci(25)
