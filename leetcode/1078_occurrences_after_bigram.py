904ed96a7fa93115125663b5af190d27cd26ef6a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-12 16:51:33 +0200

    leetcode: add solution to problem 1078

---
+++ leetcode/1078_occurrences_after_bigram.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        result = []
+
+        words = text.split()
+        for i in xrange(len(words) - 2):
+            if words[i] == first and words[i + 1] == second:
+                result.append(words[i + 2])
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert ['girl', 'student'] == solution.findOcurrences('alice is a good girl she is a good student', 'a', 'good')
+    assert ['we', 'rock'] == solution.findOcurrences('we will we will rock you', 'we', 'will')
