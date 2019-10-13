4a842e03f1fbf2130aea3b88ef8921f45c670eef
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-13 13:06:59 +0200

    leetcode: add solution to problem 1221

---
+++ leetcode/1221_split_a_string_in_balanced_strings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def balancedStringSplit(self, s):
+        r_count, result = 0, 0
+
+        for char in s:
+            r_count += 1 if char == 'R' else -1
+            if r_count == 0:
+                result += 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 4 == solution.balancedStringSplit("RLRRLLRLRL")
+    assert 3 == solution.balancedStringSplit("RLLLLRRRLR")
+    assert 1 == solution.balancedStringSplit("LLLLRRRR")
