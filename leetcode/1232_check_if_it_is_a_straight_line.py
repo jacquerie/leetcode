fbb97c41d3dc251bd7c448eb76b5e3d8bc7fc2d9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-20 13:36:46 +0200

    leetcode: add solution to problem 1232

---
+++ leetcode/1232_check_if_it_is_a_straight_line.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        (x0, y0), (x1, y1) = coordinates[0], coordinates[1]
+
+        m = (y1 - y0) / (x1 - x0) if x1 != x0 else 0
+        q = y0 - m * x0
+
+        return all(y == m * x + q for x, y in coordinates)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]])
+    assert not solution.checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]])
