3abb2aea265d65b8138fe4b417f79de148c30125
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-20 03:21:58 +0200

    leetcode: add solution to problem 1228

---
+++ leetcode/1228_missing_number_in_arithmetic_progression.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def missingNumber(self, arr):
+        return ((arr[-1] + arr[0]) * (len(arr) + 1)) // 2 - sum(arr)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 9 == solution.missingNumber([5, 7, 11, 13])
+    assert 14 == solution.missingNumber([15, 13, 12])
+    assert 0 == solution.missingNumber([0, 0, 0, 0, 0])
