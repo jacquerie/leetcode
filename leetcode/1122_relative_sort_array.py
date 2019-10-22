f9b4a78e22bea4fc70d311982177186ea43e3615
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-22 22:00:52 +0200

    leetcode: add solution to problem 1122

---
+++ leetcode/1122_relative_sort_array.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        ranks = {el: i for i, el in enumerate(arr2)}
+        return sorted(arr1, key=lambda el: (ranks.get(el, float('inf')), el))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19] == solution.relativeSortArray([2, 3, 1, 3, 2, 4, 6, 19, 9, 2, 7], [2, 1, 4, 3, 9, 6])
