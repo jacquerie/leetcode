fa434d24a575b43fa9f2ff7176f77c76445769e4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-23 04:20:27 +0200

    leetcode: improve solution to problem 0645

--- leetcode/0645_set_mismatch.py
+++ leetcode/0645_set_mismatch.py
@@ -2,18 +2,27 @@
 
 from __future__ import absolute_import, division, print_function
 
-from collections import Counter
-
 
 class Solution(object):
     def findErrorNums(self, nums):
-        repeated = (Counter(nums) - Counter(range(1, len(nums) + 1))).keys()
-        missing = list(set(range(1, len(nums) + 1)) - set(nums))
+        result = []
 
-        return repeated + missing
+        for num in nums:
+            i = abs(num) - 1
+            if nums[i] < 0:
+                result.append(i + 1)
+            nums[i] = -nums[i]
+
+        for i, num in enumerate(nums, 1):
+            if num > 0 and i not in result:
+                result.append(i)
+                break
+
+        return result
 
 
 if __name__ == '__main__':
     solution = Solution()
 
     assert [2, 3] == solution.findErrorNums([1, 2, 2, 4])
+    assert [2, 1] == solution.findErrorNums([2, 2])
