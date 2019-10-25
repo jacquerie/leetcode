23eb418b8983c540de98485b1fbd616b99d84f0e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-10-25 23:04:22 +0200

    leetcode: add solution to problem 1046

---
+++ leetcode/1046_last_stone_weight.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from heapq import heappop, heappush
+
+
+class MaxHeap(object):
+    def __init__(self):
+        self.count = 0
+        self.els = []
+
+    def __len__(self):
+        return self.count
+
+    def _max(self):
+        _, el = self.els[0]
+        return el
+
+    def pop(self):
+        self.count -= 1
+        _, el = heappop(self.els)
+        return el
+
+    def push(self, el):
+        self.count += 1
+        heappush(self.els, (-el, el))
+
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        heap = MaxHeap()
+        for stone in stones:
+            heap.push(stone)
+
+        while len(heap) > 1:
+            x = heap.pop()
+            y = heap.pop()
+            if x > y:
+                heap.push(x - y)
+
+        return heap.pop() if len(heap) == 1 else 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.lastStoneWeight([2, 7, 4, 1, 8, 1])
+    assert 0 == solution.lastStoneWeight([2, 2])
