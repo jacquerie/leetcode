8cf986c015110e1bfccbe05a8a81ac3f29cc14dd
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-11-03 05:28:39 +0100

    leetcode: add solution to problem 1244

---
+++ leetcode/1244_design_a_leaderboard.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Leaderboard(object):
+    def __init__(self):
+        self.scores = Counter()
+
+    def addScore(self, playerId, score):
+        self.scores[playerId] += score
+
+    def top(self, K):
+        return sum(score for _, score in self.scores.most_common(K))
+
+    def reset(self, playerId):
+        self.scores[playerId] = 0
+
+
+if __name__ == '__main__':
+    obj = Leaderboard()
+
+    obj.addScore(1, 73)
+    obj.addScore(2, 56)
+    obj.addScore(3, 39)
+    obj.addScore(4, 51)
+    obj.addScore(5, 4)
+    assert 73 == obj.top(1)
+    obj.reset(1)
+    obj.reset(2)
+    obj.addScore(2, 51)
+    assert 141 == obj.top(3)
