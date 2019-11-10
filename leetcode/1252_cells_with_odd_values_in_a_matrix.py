1b49cbc9c6263ec5904d1d4d654836765cfcf413
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-11-10 23:08:26 +0100

    leetcode: add solution to problem 1252

---
+++ leetcode/1252_cells_with_odd_values_in_a_matrix.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from collections import Counter
+
+
+class Solution(object):
+    def oddCells(self, n, m, indices):
+        row_counts = Counter()
+        col_counts = Counter()
+        for row, col in indices:
+            row_counts[row] += 1
+            col_counts[col] += 1
+
+        return sum((row_counts[i] + col_counts[j]) % 2 for i in xrange(n) for j in xrange(m))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.oddCells(2, 3, [[0, 1], [1, 1]])
+    assert 0 == solution.oddCells(2, 2, [[1, 1], [0, 0]])
