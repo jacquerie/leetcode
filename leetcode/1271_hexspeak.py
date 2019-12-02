20eeefb5028edb7831d15db87168fe0754bf5b21
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-12-02 01:20:55 +0100

    leetcode: add solution to problem 1271

---
+++ leetcode/1271_hexspeak.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def toHexspeak(self, num: str) -> str:
+        h = hex(int(num))[2:].replace('0', 'o').replace('1', 'i').upper()
+        if '3' in h or '4' in h or '5' in h or '6' in h or '7' in h or '8' in h or '9' in h:
+            return 'ERROR'
+        return h
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'IOI' == solution.toHexspeak('257')
+    assert 'ERROR' == solution.toHexspeak('3')
+    assert 'AEIDBCDIBC' == solution.toHexspeak('747823223228')
