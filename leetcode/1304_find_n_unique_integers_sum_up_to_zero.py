629767dd9a6ccabcb7619d7397f70a4aadb3bece
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-12-31 19:40:56 +0100

    leetcode: add solution to problem 1304

---
+++ leetcode/1304_find_n_unique_integers_sum_up_to_zero.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        result = [] if n % 2 == 0 else [0]
+        for i in range(1, n // 2 + 1):
+            result.append(i)
+            result.append(-i)
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [0, 1, -1, 2, -2] == solution.sumZero(5)
+    assert [0, 1, -1] == solution.sumZero(3)
+    assert [0] == solution.sumZero(1)
