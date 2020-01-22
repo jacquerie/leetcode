5b3e8a047ed54af59b5d920babede3a151741aba
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-01-22 21:26:24 +0100

    leetcode: add solution to problem 1005

---
+++ leetcode/1005_maximize_sum_of_array_after_k_negations.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from heapq import heappop, heappush
+from typing import List
+
+
+class MinHeap:
+    def __init__(self):
+        self.els = []
+
+    def pop(self):
+        return heappop(self.els)
+
+    def push(self, el):
+        heappush(self.els, el)
+
+
+class Solution:
+    def largestSumAfterKNegations(self, A: List[int], K: int) -> int:
+        heap = MinHeap()
+        for el in A:
+            heap.push(el)
+
+        for _ in range(K):
+            el = heap.pop()
+            heap.push(-el)
+
+        return sum(heap.els)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.largestSumAfterKNegations([4, 2, 3], 1)
+    assert 6 == solution.largestSumAfterKNegations([3, -1, 0, 2], 3)
+    assert 13 == solution.largestSumAfterKNegations([2, -3, -1, 5, -4], 2)
