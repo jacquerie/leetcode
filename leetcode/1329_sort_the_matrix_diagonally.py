ee06858b1dd4c8e98b00988c4618b75f5e506ee0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-02-02 05:16:32 +0100

    leetcode: add solution to problem 1329

---
+++ leetcode/1329_sort_the_matrix_diagonally.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        diags = defaultdict(list)
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                diags[i - j].append(mat[i][j])
+
+        for diag in diags.values():
+            diag.sort(reverse=True)
+
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                mat[i][j] = diags[i - j].pop()
+
+        return mat
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [
+        [1, 1, 1, 1],
+        [1, 2, 2, 2],
+        [1, 2, 3, 3],
+    ] == solution.diagonalSort([
+        [3, 3, 1, 1],
+        [2, 2, 1, 2],
+        [1, 1, 1, 2],
+    ])
