5663416dba00a1865d5f5e86f94b055a7ae13d2a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-02-03 01:44:23 +0100

    leetcode: add solution to problem 1337

---
+++ leetcode/1337_the_k_weakest_rows_in_a_matrix.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        return [i for _, i in sorted((sum(row), i) for i, row in enumerate(mat))[:k]]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [2, 0, 3] == solution.kWeakestRows([
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ], 3)
+    assert [0, 2] == solution.kWeakestRows([
+        [1, 0, 0, 0],
+        [1, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+    ], 2)
