5d8d9cc920a2124ff71aac0ef1e225adf79c8dfe
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-02-09 21:01:45 +0100

    leetcode: add solution to problem 1342

---
+++ leetcode/1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        result = 0
+
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            result += 1
+
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.numberOfSteps(14)
+    assert 4 == solution.numberOfSteps(8)
+    assert 12 == solution.numberOfSteps(123)
