3e198f79dde1370aeb3243f714c995c093a55dff
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-03-08 01:31:28 +0100

    leetcode: add solution to problem 1370

---
+++ leetcode/1370_increasing_decreasing_string.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        result = []
+
+        counts = sorted([c, count] for c, count in Counter(s).items())
+        while len(result) < len(s):
+            for i, (c, count) in enumerate(counts):
+                if count > 0:
+                    result.append(c)
+                    counts[i][1] -= 1
+            for i, (c, count) in enumerate(reversed(counts)):
+                if count > 0:
+                    result.append(c)
+                    counts[~i][1] -= 1
+
+        return ''.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'abccbaabccba' == solution.sortString('aaaabbbbcccc')
+    assert 'art' == solution.sortString('rat')
+    assert 'cdelotee' == solution.sortString('leetcode')
+    assert 'ggggggg' == solution.sortString('ggggggg')
