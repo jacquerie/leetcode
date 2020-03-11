780bdd09ed099027f4f36c3ddae11947181e5aea
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-03-11 02:37:58 +0100

    leetcode: add solution to problem 1374

---
+++ leetcode/1374_generate_a_string_with_characters_that_have_odd_counts.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n % 2:
+            return 'a' * n
+        return 'a' * (n - 1) + 'b'
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'aaab' == solution.generateTheString(4)
+    assert 'ab' == solution.generateTheString(2)
+    assert 'aaaaaaa' == solution.generateTheString(7)
