991bf35be43860bd6fcb5a8ed1665f150fd1bf8a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-03-12 07:48:00 +0100

    leetcode: add solution to problem 1010

---
+++ leetcode/1010_pairs_of_songs_with_total_duration_divisible_by_60.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        result = 0
+
+        counts = Counter(el % 60 for el in time)
+        for el, count in counts.items():
+            if el == 0 or el == 30:
+                result += count * (count - 1)
+            else:
+                result += count * counts[60 - el]
+
+        return result // 2
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.numPairsDivisibleBy60([30, 20, 150, 100, 40])
+    assert 3 == solution.numPairsDivisibleBy60([60, 60, 60])
