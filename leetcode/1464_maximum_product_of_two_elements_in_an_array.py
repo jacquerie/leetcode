d5e50f8ec0e01be2a5ff01d3ba3575be0cfab0fd
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-05-31 07:33:35 +0200

    leetcode: add solution to problem 1464

---
+++ leetcode/1464_maximum_product_of_two_elements_in_an_array.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        return max((x - 1) * (y - 1) for x, y in itertools.combinations(nums, 2))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 12 == solution.maxProduct([3, 4, 5, 2])
+    assert 16 == solution.maxProduct([1, 5, 4, 5])
+    assert 12 == solution.maxProduct([3, 7])
