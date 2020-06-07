5ee4b1f6240c97793637e607d44f2784f8c6d18f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-06-07 08:53:58 +0200

    leetcode: add solution to problem 1431

---
+++ leetcode/1431_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [candy + extraCandies >= max(candies) for candy in candies]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [True, True, True, False, True] == solution.kidsWithCandies([2, 3, 5, 1, 3], 3)
+    assert [True, False, False, False, False] == solution.kidsWithCandies([4, 2, 1, 1, 2], 1)
+    assert [True, False, True] == solution.kidsWithCandies([12, 1, 12], 10)
