ae75aad4ee07210bb47f3795c6e6a167fb9466be
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-06-29 06:59:30 +0200

    leetcode: add solution to problem 1480

---
+++ leetcode/1480_running_sum_of_1d_array.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return list(itertools.accumulate(nums))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [1, 3, 6, 10] == solution.runningSum([1, 2, 3, 4])
+    assert [1, 2, 3, 4, 5] == solution.runningSum([1, 1, 1, 1, 1])
+    assert [3, 4, 6, 16, 17] == solution.runningSum([3, 1, 2, 10, 1])
