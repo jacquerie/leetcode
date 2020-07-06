e7f1c7dff6563413ce9586406ae90021d1083ad0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-06 02:10:23 +0200

    leetcode: add solution to problem 1491

---
+++ leetcode/1491_average_salary_excluding_the_minimum_and_maximum_salary.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        return (sum(salary) - min(salary) - max(salary)) / (len(salary) - 2)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2500.0 == solution.average([4000, 3000, 1000, 2000])
+    assert 2000.0 == solution.average([1000, 2000, 3000])
+    assert 3500.0 == solution.average([6000, 5000, 4000, 3000, 2000, 1000])
+    assert 4750.0 == solution.average([8000, 9000, 2000, 3000, 6000, 1000])
