1de216826f6165717d0227ed84be64d51ab3589a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-12 23:10:30 +0200

    leetcode: add solution to problem 1512

---
+++ leetcode/1512_number_of_good_pairs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        return sum(count * (count - 1) // 2 for _, count in Counter(nums).items())
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 4 == solution.numIdenticalPairs([1, 2, 3, 1, 1, 3])
+    assert 6 == solution.numIdenticalPairs([1, 1, 1, 1])
+    assert 0 == solution.numIdenticalPairs([1, 2, 3])
