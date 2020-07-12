a82c2ae9341e2c56c9aee3f1c7280368591402a4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-12 23:57:03 +0200

    leetcode: add solution to problem 1496

---
+++ leetcode/1496_path_crossing.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x, y, visited = 0, 0, set([(0, 0)])
+        for char in path:
+            if char == 'N':
+                x += 1
+            elif char == 'E':
+                y += 1
+            elif char == 'S':
+                x -= 1
+            elif char == 'W':
+                y -= 1
+            if (x, y) in visited:
+                return True
+            visited.add((x, y))
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert not solution.isPathCrossing('NES')
+    assert solution.isPathCrossing('NESWW')
