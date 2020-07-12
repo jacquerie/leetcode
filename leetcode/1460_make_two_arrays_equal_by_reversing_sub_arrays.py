d1034140c8d847552a9115229196a536fb5643e5
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-12 23:45:18 +0200

    leetcode: add solution to problem 1460

---
+++ leetcode/1460_make_two_arrays_equal_by_reversing_sub_arrays.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return Counter(target) == Counter(arr)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.canBeEqual([1, 2, 3, 4], [2, 4, 1, 3])
+    assert solution.canBeEqual([7], [7])
+    assert solution.canBeEqual([1, 12], [12, 1])
+    assert not solution.canBeEqual([3, 7, 9], [3, 7, 11])
+    assert solution.canBeEqual([1, 1, 1, 1, 1], [1, 1, 1, 1, 1])
