656836d1b66cd5a5efc3865d39104b5eb51fcd99
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-13 00:43:21 +0200

    leetcode: add solution to problem 1475

---
+++ leetcode/1475_final_prices_with_a_special_discount_in_a_shop.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        result = []
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] <= prices[i]:
+                    result.append(prices[i] - prices[j])
+                    break
+            else:
+                result.append(prices[i])
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [4, 2, 4, 2, 3] == solution.finalPrices([8, 4, 6, 2, 3])
+    assert [1, 2, 3, 4, 5] == solution.finalPrices([1, 2, 3, 4, 5])
+    assert [9, 0, 1, 6] == solution.finalPrices([10, 1, 1, 6])
