54fc9f80774be9fffd6aa5f5d8cbdedb9c340708
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-22 22:33:57 +0200

    leetcode: add solution to problem 1403

---
+++ leetcode/1403_minimum_subsequence_in_non_increasing_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        total_sum = sum(nums)
+        nums.sort(reverse=True)
+
+        current_sum = 0
+        for i, num in enumerate(nums):
+            current_sum += num
+            if 2 * current_sum > total_sum:
+                return nums[:i + 1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert [10, 9] == solution.minSubsequence([4, 3, 10, 9, 8])
+    assert [7, 7, 6] == solution.minSubsequence([4, 4, 7, 6, 7])
+    assert [6] == solution.minSubsequence([6])
