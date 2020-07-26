bd63d0be823eb6a1b9a3c56650adb33b3252252c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-07-26 04:10:57 +0200

    leetcode: add solution to problem 1446

---
+++ leetcode/1446_consecutive_characters.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        result = 0
+        for _, group in itertools.groupby(s):
+            result = max(result, len(list(group)))
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.maxPower('leetcode')
+    assert 5 == solution.maxPower('abbcccddddeeeeedcba')
+    assert 5 == solution.maxPower('triplepillooooow')
+    assert 11 == solution.maxPower('hooraaaaaaaaaaay')
+    assert 1 == solution.maxPower('tourist')
