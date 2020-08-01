45fdfe0e1da9980a1418bf0da187ba7138cbf6f4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-08-02 01:39:38 +0200

    leetcode: add solution to problem 1029

---
+++ leetcode/1029_two_city_scheduling.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        differences, result = [], 0
+        for a_cost, b_cost in costs:
+            differences.append(a_cost - b_cost)
+            result += a_cost
+        differences.sort(reverse=True)
+        return result - sum(differences[:len(costs) // 2])
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 110 == solution.twoCitySchedCost([[10, 20], [30, 200], [400, 50], [30, 20]])
