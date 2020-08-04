aa7640536da67c40b1569f0fe2cf008be20f05bd
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-08-05 00:58:39 +0200

    leetcode: add solution to problem 1534

---
+++ leetcode/1534_count_good_triplets.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+from typing import List
+
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        return sum(abs(x - y) <= a and abs(y - z) <= b and abs(x - z) <= c for x, y, z in itertools.combinations(arr, 3))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 4 == solution.countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3)
+    assert 0 == solution.countGoodTriplets([1, 1, 2, 2, 3], 0, 0, 1)
