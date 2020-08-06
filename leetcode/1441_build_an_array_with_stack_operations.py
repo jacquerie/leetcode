79423178ba3fed1dc6631df5ba063734cd2dd810
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-08-07 01:00:10 +0200

    leetcode: add solution to problem 1441

---
+++ leetcode/1441_build_an_array_with_stack_operations.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        result = []
+        for el in range(1, target[-1] + 1):
+            result.append('Push')
+            if el not in target:
+                result.append('Pop')
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert ['Push', 'Push', 'Pop', 'Push'] == solution.buildArray([1, 3], 3)
+    assert ['Push', 'Push', 'Push'] == solution.buildArray([1, 2, 3], 3)
+    assert ['Push', 'Push'] == solution.buildArray([1, 2], 4)
+    assert ['Push', 'Pop', 'Push', 'Push', 'Push'] == solution.buildArray([2, 3, 4], 4)
