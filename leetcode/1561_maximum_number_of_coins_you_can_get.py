ac51ba3f08415277023b6c9172896668e0ffd873
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-10-18 04:24:51 +0200

    leetcode: add solution to problem 1561

---
+++ leetcode/1561_maximum_number_of_coins_you_can_get.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        return sum(pile for i, pile in enumerate(sorted(piles, reverse=True)) if i % 2 == 1 and i <= 2 * len(piles) / 3)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 9 == solution.maxCoins([2, 4, 1, 2, 7, 8])
+    assert 4 == solution.maxCoins([2, 4, 5])
+    assert 18 == solution.maxCoins([9, 8, 7, 6, 5, 1, 2, 3, 4])
