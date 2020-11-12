6bd8c3a61631a49ba30cadc1dbdfd0c1d0007a63
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-11-12 03:02:58 +0100

    leetcode: add solution to problem 1646

---
+++ leetcode/1646_get_maximum_in_generated_array.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        nums, result = [0] * (n + 1), 0
+        for i in range(1, n + 1):
+            if i == 1:
+                nums[i] = 1
+            elif i % 2 == 0:
+                nums[i] = nums[i // 2]
+            elif i % 2 == 1:
+                nums[i] = nums[i // 2] + nums[i // 2 + 1]
+            result = max(result, nums[i])
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.getMaximumGenerated(7)
+    assert 1 == solution.getMaximumGenerated(2)
+    assert 2 == solution.getMaximumGenerated(3)
