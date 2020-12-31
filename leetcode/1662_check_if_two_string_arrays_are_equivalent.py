515c31784ca983518d12823589aba60e501abb93
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2020-12-31 04:47:17 +0100

    leetcode: add solution to problem 1662

---
+++ leetcode/1662_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.arrayStringsAreEqual(['ab', 'c'], ['a', 'bc'])
+    assert not solution.arrayStringsAreEqual(['a', 'cb'], ['ab', 'c'])
+    assert solution.arrayStringsAreEqual(['abc', 'd', 'defg'], ['abcddefg'])
