3bbff5fe3bd1542547a3cd14f7d74cc134e1e12e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-01-01 21:23:07 +0100

    leetcode: add solution to problem 1689

---
+++ leetcode/1689_partitioning_into_minimum_number_of_deci_binary_numbers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return max(int(c) for c in n)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.minPartitions('32')
+    assert 8 == solution.minPartitions('82374')
+    assert 9 == solution.minPartitions('27346209830709182346')
