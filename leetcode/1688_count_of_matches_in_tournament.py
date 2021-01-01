562794c042555af9a646ee89e98e5456fde635d8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-01-01 21:28:17 +0100

    leetcode: add solution to problem 1688

---
+++ leetcode/1688_count_of_matches_in_tournament.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.numberOfMatches(7)
+    assert 13 == solution.numberOfMatches(14)
