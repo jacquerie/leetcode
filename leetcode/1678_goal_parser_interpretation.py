51cdc4d3f23f6bad91dc4b59b2c1909a669e0fb6
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-01-02 19:06:50 +0100

    leetcode: add solution to problem 1678

---
+++ leetcode/1678_goal_parser_interpretation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace('()', 'o').replace('(al)', 'al')
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'Goal' == solution.interpret('G()(al)')
+    assert 'Gooooal' == solution.interpret('G()()()()(al)')
+    assert 'alGalooG' == solution.interpret('(al)G(al)()()G')
