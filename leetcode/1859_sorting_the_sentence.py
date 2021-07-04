dc4fe7ff0c6535688bb5bd9d81d7afcdd6aa1efb
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-05 00:06:23 +0200

    leetcode: add solution to problem 1859

---
+++ leetcode/1859_sorting_the_sentence.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        tokens = s.split()
+        result = [None] * len(tokens)
+
+        for token in tokens:
+            word, index = token[:-1], int(token[-1])
+            result[index - 1] = word
+
+        return ' '.join(result)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'This is a sentence' == solution.sortSentence('is2 sentence4 This1 a3')
+    assert 'Me Myself and I' == solution.sortSentence('Myself2 Me1 I4 and3')
