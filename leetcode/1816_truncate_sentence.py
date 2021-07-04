7d4080996698dd6e4e03fc55f612c8379c88feb4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-05 00:16:51 +0200

    leetcode: add solution to problem 1816

---
+++ leetcode/1816_truncate_sentence.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return ' '.join(s.split()[:k])
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'Hello how are you' == solution.truncateSentence('Hello how are you Contestant', 4)
+    assert 'What is the solution' == solution.truncateSentence('What is the solution to this problem', 4)
+    assert 'chopper is not a tanuki' == solution.truncateSentence('chopper is not a tanuki', 5)
