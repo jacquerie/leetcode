b77602e3f7cd87c34e56f61d05e754fa811193b2
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-09 01:56:10 +0200

    leetcode: add solution to problem 1704

---
+++ leetcode/1704_determine_if_string_halves_are_alike.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        return self.countVowels(s[:len(s) // 2]) == self.countVowels(s[len(s) // 2:])
+
+    def countVowels(self, s: str) -> int:
+        counts = Counter(s.lower())
+        return counts['a'] + counts['e'] + counts['i'] + counts['o'] + counts['u']
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.halvesAreAlike('book')
+    assert not solution.halvesAreAlike('textbook')
+    assert not solution.halvesAreAlike('MerryChristmas')
+    assert solution.halvesAreAlike('AbCdEfGh')
