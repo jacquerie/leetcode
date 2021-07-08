23b3a85220ad66b78fdb46aac42b8feecb19980b
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-09 01:04:02 +0200

    leetcode: add solution to problem 1748

---
+++ leetcode/1748_sum_of_unique_elements.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        return sum(el for el, count in Counter(nums).items() if count == 1)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 4 == solution.sumOfUnique([1, 2, 3, 2])
+    assert 0 == solution.sumOfUnique([1, 1, 1, 1, 1])
+    assert 15 == solution.sumOfUnique([1, 2, 3, 4, 5])
