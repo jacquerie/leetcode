e09235ad6f3b85503435d514a32b5b68f776bc10
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-09 01:36:37 +0200

    leetcode: add solution to problem 1837

---
+++ leetcode/1837_sum_of_digits_in_base_k.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        return sum(self.toBaseK(n, k))
+
+    def toBaseK(self, n: int, k: int) -> List[int]:
+        digits = []
+        while n:
+            digits.append(n % k)
+            n //= k
+        return digits
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 9 == solution.sumBase(34, 6)
+    assert 1 == solution.sumBase(10, 10)
