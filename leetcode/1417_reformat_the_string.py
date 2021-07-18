a581cd5ea1991cbd8f719ccf0a78d93bf9d27e37
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-18 21:21:03 +0200

    leetcode: add solution to problem 1417

---
+++ leetcode/1417_reformat_the_string.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def reformat(self, s: str) -> str:
+        digits, letters = [], []
+        for char in s:
+            if char.isdigit():
+                digits.append(char)
+            else:
+                letters.append(char)
+
+        if abs(len(digits) - len(letters)) > 1:
+            return ''
+
+        result = []
+
+        more_digits_than_letters = len(digits) > len(letters)
+        while digits or letters:
+            char = digits.pop() if more_digits_than_letters else letters.pop()
+            more_digits_than_letters = not more_digits_than_letters
+            result.append(char)
+
+        return ''.join(reversed(result))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert '0a1b2c' == solution.reformat('a0b1c2')
+    assert '' == solution.reformat('leetcode')
+    assert '' == solution.reformat('1229857369')
+    assert 'c2o0v1i9d' == solution.reformat('covid2019')
+    assert '1a2b3' == solution.reformat('ab123')
