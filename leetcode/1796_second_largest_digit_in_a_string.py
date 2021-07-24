e513e6b41e485fd2b8e90c426e09f58d072051c9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-24 09:58:18 +0200

    leetcode: add solution to problem 1796

---
+++ leetcode/1796_second_largest_digit_in_a_string.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        digits = {int(c) for c in s if c.isdigit()}
+        if len(digits) < 2:
+            return -1
+        *_, result, _ = sorted(list(digits))
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.secondHighest('dfa12321afd')
+    assert -1 == solution.secondHighest('abc1111')
+    assert 0 == solution.secondHighest('ck077')
