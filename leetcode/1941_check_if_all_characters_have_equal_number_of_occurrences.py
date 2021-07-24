614940a6d8a98d5f4161b7ff017618109104b211
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-24 23:57:47 +0200

    leetcode: add solution to problem 1941

---
+++ leetcode/1941_check_if_all_characters_have_equal_number_of_occurrences.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        return len(set(Counter(s).values())) == 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.areOccurrencesEqual('abcabc')
+    assert not solution.areOccurrencesEqual('aaabb')
