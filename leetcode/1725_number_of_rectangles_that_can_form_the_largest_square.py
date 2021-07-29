c2921b212f28f511a12ba734384e5458bf6145a4
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-07-29 11:47:06 +0200

    leetcode: add solution to problem 1725

---
+++ leetcode/1725_number_of_rectangles_that_can_form_the_largest_square.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+        max_square_side_length, result = float('-inf'), 0
+        for length, width in rectangles:
+            square_side_length = min(length, width)
+            if square_side_length > max_square_side_length:
+                max_square_side_length, result = square_side_length, 1
+            elif square_side_length == max_square_side_length:
+                result += 1
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 3 == solution.countGoodRectangles([[5, 8], [3, 9], [5, 12], [16, 5]])
+    assert 3 == solution.countGoodRectangles([[2, 3], [3, 7], [4, 3], [3, 7]])
