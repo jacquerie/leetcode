cd93ae673976399c53114ad5516c400d959644a0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-08-04 03:27:29 +0200

    leetcode: add solution to problem 1925

---
+++ leetcode/1925_count_square_sum_triples.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+
+
+class Solution:
+    SQUARES = {i * i for i in range(1, 251)}
+
+    def countTriples(self, n: int) -> int:
+        result = 0
+        for a, b in itertools.product(range(1, n + 1), repeat=2):
+            sum_of_squares = a * a + b * b
+            if sum_of_squares in self.SQUARES and sum_of_squares <= n * n:
+                result += 1
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.countTriples(5)
+    assert 4 == solution.countTriples(10)
+    assert 4 == solution.countTriples(12)
