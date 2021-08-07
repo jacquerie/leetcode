2c6559a67ec8b7070e1f6bfe4ebd26c7b662e2c0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-08-07 02:46:15 +0200

    leetcode: add solution to problem 1848

---
+++ leetcode/1848_minimum_distance_to_the_target_element.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        return min(abs(i - start) for i, el in enumerate(nums) if el == target)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.getMinDistance([1, 2, 3, 4, 5], 5, 3)
+    assert 0 == solution.getMinDistance([1], 1, 0)
+    assert 0 == solution.getMinDistance([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1, 0)
