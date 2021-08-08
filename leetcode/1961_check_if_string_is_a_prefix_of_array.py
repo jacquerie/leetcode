d603bbfea5811b62cb77f66e1d10aeb9cca7d23f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-08-08 20:20:12 +0200

    leetcode: add solution to problem 1961

---
+++ leetcode/1961_check_if_string_is_a_prefix_of_array.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        current_prefix = ''
+        for word in words:
+            current_prefix += word
+            if current_prefix == s:
+                return True
+            elif len(current_prefix) > len(s):
+                return False
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isPrefixString('iloveleetcode', ['i', 'love', 'leetcode', 'apples'])
+    assert not solution.isPrefixString('iloveleetcode', ['apples', 'i', 'love', 'leetcode'])
+    assert not solution.isPrefixString('a', ['aa', 'aaaa', 'banana'])
