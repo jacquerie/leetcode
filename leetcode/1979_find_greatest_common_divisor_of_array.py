31ef1a7397fe4f9dd0f21c5dcd95d88b0096dba0
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-03 21:53:46 +0200

    leetcode: add solution to problem 1979

---
+++ leetcode/1979_find_greatest_common_divisor_of_array.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import math
+from typing import List
+
+
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        return math.gcd(min(nums), max(nums))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.findGCD([2, 5, 6, 9, 10])
+    assert 1 == solution.findGCD([7, 5, 6, 8, 3])
+    assert 3 == solution.findGCD([3, 3])
