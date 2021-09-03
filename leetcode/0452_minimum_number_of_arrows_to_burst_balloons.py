21ca21715993bf164797f5b0d48d1e6218d973a7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-04 01:31:51 +0200

    leetcode: add solution to problem 0452

---
+++ leetcode/0452_minimum_number_of_arrows_to_burst_balloons.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda point: point[1])
+
+        current_end, result = float('-inf'), len(points)
+        for point in points:
+            if point[0] > current_end:
+                current_end = point[1]
+            else:
+                result -= 1
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]])
+    assert 4 == solution.findMinArrowShots([[1, 2], [3, 4], [5, 6], [7, 8]])
+    assert 2 == solution.findMinArrowShots([[1, 2], [2, 3], [3, 4], [4, 5]])
