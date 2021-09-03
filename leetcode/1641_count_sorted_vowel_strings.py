14123a280e7df7982b9150eacc4aa23356060fb9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-04 00:17:46 +0200

    leetcode: add solution to problem 1641

---
+++ leetcode/1641_count_sorted_vowel_strings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        return math.comb(n + 4, n)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 5 == solution.countVowelStrings(1)
+    assert 15 == solution.countVowelStrings(2)
+    assert 66045 == solution.countVowelStrings(33)
