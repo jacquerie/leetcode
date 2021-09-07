a24c321248e6a1dec21ee73acf6b076341750dd9
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-07 23:30:36 +0200

    leetcode: add solution to problem 1995

---
+++ leetcode/1995_count_special_quadruplets.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+from typing import List
+
+
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        return sum(a + b + c == d for a, b, c, d in itertools.combinations(nums, 4))
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 1 == solution.countQuadruplets([1, 2, 3, 6])
+    assert 0 == solution.countQuadruplets([3, 3, 6, 4, 5])
+    assert 4 == solution.countQuadruplets([1, 1, 1, 3, 5])
