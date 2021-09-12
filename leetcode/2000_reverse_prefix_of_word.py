e1f849d0234c276cd03cdd00afee7ab7dd91b9c3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-12 21:32:52 +0200

    leetcode: add solution to problem 2000

---
+++ leetcode/2000_reverse_prefix_of_word.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        return ''.join(reversed(word[:word.find(ch) + 1])) + word[word.find(ch) + 1:]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'dcbaefd' == solution.reversePrefix('abcdefd', 'd')
+    assert 'zxyxxe' == solution.reversePrefix('xyxzxe', 'z')
+    assert 'abcd' == solution.reversePrefix('abcd', 'z')
