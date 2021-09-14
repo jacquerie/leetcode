f928a3085b49e32f3db4c78cddbaf5afa97f9bcc
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-14 12:27:01 +0200

    leetcode: add solution to problem 1877

---
+++ leetcode/1877_minimize_maximum_pair_sum_in_array.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        i, result = 0, float('-inf')
+        while i <= len(nums) // 2:
+            i, result = i + 1, max(result, nums[i] + nums[-i - 1])
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 7 == solution.minPairSum([3, 5, 2, 3])
+    assert 8 == solution.minPairSum([3, 5, 4, 2, 4, 6])
