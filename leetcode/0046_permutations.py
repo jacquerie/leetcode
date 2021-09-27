87a63532710e3b012529c6a3b672cbc37c212d16
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-27 18:59:44 +0200

    leetcode: improve solution to problem 0046

--- leetcode/0046_permutations.py
+++ leetcode/0046_permutations.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import itertools
+from typing import List
 
 
 class Solution:
-    def permute(self, nums):
-        return [list(el) for el in itertools.permutations(nums)]
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self._permute(nums, [], result)
+        return result
+
+    def _permute(self, nums: List[int], current: List[int], result: List[List[int]]):
+        if not nums:
+            result.append(current)
+        for i in range(len(nums)):
+            self._permute(nums[:i] + nums[i + 1:], current + [nums[i]], result)
 
 
 if __name__ == '__main__':
@@ -19,3 +27,5 @@ if __name__ == '__main__':
         [3, 1, 2],
         [3, 2, 1],
     ] == solution.permute([1, 2, 3])
+    assert [[0, 1], [1, 0]] == solution.permute([0, 1])
+    assert [[1]] == solution.permute([1])
