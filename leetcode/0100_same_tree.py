28b894b35c7b54a7497eab1b5665d79172ebb010
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-09-27 17:09:58 +0200

    leetcode: improve solution to problem 0100

--- leetcode/0100_same_tree.py
+++ leetcode/0100_same_tree.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from typing import Optional
+
 
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def isSameTree(self, p, q):
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p is None and q is None:
             return True
         elif p is None or q is None:
             return False
-
         return (
             p.val == q.val and
             self.isSameTree(p.left, q.left) and
@@ -25,16 +26,12 @@ class Solution:
 if __name__ == '__main__':
     solution = Solution()
 
-    t0_0 = TreeNode(1)
-    t0_1 = TreeNode(2)
     t0_2 = TreeNode(3)
-    t0_0.left = t0_1
-    t0_0.right = t0_2
+    t0_1 = TreeNode(2)
+    t0_0 = TreeNode(1, t0_1, t0_2)
 
-    t1_0 = TreeNode(1)
-    t1_1 = TreeNode(2)
     t1_2 = TreeNode(3)
-    t1_0.left = t1_1
-    t1_0.right = t1_2
+    t1_1 = TreeNode(2)
+    t1_0 = TreeNode(1, t1_1, t1_2)
 
     assert solution.isSameTree(t0_0, t1_0)
