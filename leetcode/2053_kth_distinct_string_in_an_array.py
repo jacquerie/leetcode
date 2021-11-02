972c2469fa78b28c3baa3022417629fbbbf896e3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-11-02 18:45:49 +0100

    leetcode: add solution to problem 2053

---
+++ leetcode/2053_kth_distinct_string_in_an_array.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        seen, seen_twice = set(), set()
+        for el in arr:
+            if el in seen:
+                seen_twice.add(el)
+            seen.add(el)
+
+        for el in arr:
+            if el not in seen_twice and k > 1:
+                k -= 1
+            elif el not in seen_twice:
+                return el
+        return ''
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 'a' == solution.kthDistinct(['d', 'b', 'c', 'b', 'c', 'a'], 2)
+    assert 'aaa' == solution.kthDistinct(['aaa', 'aa', 'a'], 1)
+    assert '' == solution.kthDistinct(['a', 'b', 'a'], 3)
