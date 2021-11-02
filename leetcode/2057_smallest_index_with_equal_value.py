a05e6b832eb0e0740aaff7b2eb3109038ad404bf
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2021-11-02 22:36:45 +0100

    leetcode: add solution to problem 2057

---
+++ leetcode/2057_smallest_index_with_equal_value.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i, num in enumerate(nums):
+            if i % 10 == num:
+                return i
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 0 == solution.smallestEqual([0, 1, 2])
+    assert 2 == solution.smallestEqual([4, 3, 2, 1])
+    assert -1 == solution.smallestEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+    assert 1 == solution.smallestEqual([2, 1, 3, 5, 2])
