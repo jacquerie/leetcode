4e888af36abf2e247c76b5cab511e88182491623
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2022-02-02 21:47:15 +0100

    leetcode: add solution to problem 2114

---
+++ leetcode/2114_maximum_number_of_words_found_in_sentences.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return 1 + max(sentence.count(' ') for sentence in sentences)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 6 == solution.mostWordsFound(['alice and bob love leetcode', 'i think so too', 'this is great thanks very much'])
+    assert 3 == solution.mostWordsFound(['please wait', 'continue to fight', 'continue to win'])
