397d02b428e32b7aaf4a6dd7a95d49344d136b65
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2022-02-03 01:28:36 +0100

    leetcode: add solution to problem 2154

---
+++ leetcode/2154_keep_multiplying_found_values_by_two.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums_set = set(nums)
+        while original in nums_set:
+            original *= 2
+        return original
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 24 == solution.findFinalValue([5, 3, 6, 1, 12], 3)
+    assert 4 == solution.findFinalValue([2, 7, 9], 4)
