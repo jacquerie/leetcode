b2b7733f2b609606857ac90bd5c388ac8bf47b84
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2022-02-03 02:10:15 +0100

    leetcode: add solution to problem 2148

---
+++ leetcode/2148_count_elements_with_strictly_smaller_and_greater_elements.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+    def countElements(self, nums: List[int]) -> int:
+        min_num, max_num = min(nums), max(nums)
+        return sum(min_num < num < max_num for num in nums)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert 2 == solution.countElements([11, 7, 2, 15])
+    assert 2 == solution.countElements([-3, 3, 3, 90])
