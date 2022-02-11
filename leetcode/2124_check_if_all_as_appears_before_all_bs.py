7f6333007e50b92e6b8f5b40fa697534bd0bdab5
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2022-02-11 04:33:07 +0100

    leetcode: add solution to problem 2124

---
+++ leetcode/2124_check_if_all_as_appears_before_all_bs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def checkString(self, s: str) -> bool:
+        b_seen = False
+        for char in s:
+            if char == 'a' and b_seen:
+                return False
+            if char == 'b':
+                b_seen = True
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.checkString('aaabbb')
+    assert not solution.checkString('abab')
+    assert solution.checkString('bbb')
